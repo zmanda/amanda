175d0984b2f3617f8138500a80887b2f2a74be6d
Jean-Louis Martineau <martineau@zmanda.com>
2006-11-29 12:07:18 +0000

    * restore-src/restore.c (search_tapes): Fix prompt_out setting.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@128 a8d146d6-cc15-0410-8900-af154a0219e0

--- restore-src/restore.c
+++ restore-src/restore.c
@@ -1692,13 +1692,13 @@ search_tapes(
     seentapes_t *seentapes = NULL;
     int ret;
 
+    if(!prompt_out) prompt_out = stderr;
+
     dbprintf(("search_tapes(prompt_out=%d, prompt_in=%d,  use_changer=%d, "
 	      "tapelist=%p, "
 	      "match_list=%p, flags=%p, features=%p)\n",
 	      fileno(prompt_out), fileno(prompt_in), use_changer, tapelist,
 	      match_list, flags, their_features));
-
-    if(!prompt_out) prompt_out = stderr;
 
     if(flags->blocksize)
 	blocksize = (size_t)flags->blocksize;
