b565fc11dbf9e8b8184d095f5ac1eda3a2418ed1
Jean-Louis Martineau <martineau@zmanda.com>
2007-01-11 13:06:04 +0000

    * common-src/conffile.c Set default length/filemark correctly. * man/xml-source/amanda.conf.5.xml: Document they are in kbytes.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@163 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -1830,8 +1830,8 @@ init_tapetype_defaults(void)
     conf_init_string(&tpcur.value[TAPETYPE_COMMENT]  , "");
     conf_init_string(&tpcur.value[TAPETYPE_LBL_TEMPL], "");
     conf_init_size  (&tpcur.value[TAPETYPE_BLOCKSIZE], DISK_BLOCK_KB);
-    conf_init_am64  (&tpcur.value[TAPETYPE_LENGTH]   , (off_t)2000 * 1024);
-    conf_init_am64  (&tpcur.value[TAPETYPE_FILEMARK] , (off_t)1000);
+    conf_init_am64  (&tpcur.value[TAPETYPE_LENGTH]   , (off_t)2000);
+    conf_init_am64  (&tpcur.value[TAPETYPE_FILEMARK] , (off_t)1);
     conf_init_int   (&tpcur.value[TAPETYPE_SPEED]    , 200);
     conf_init_bool  (&tpcur.value[TAPETYPE_FILE_PAD] , 1);
 }
