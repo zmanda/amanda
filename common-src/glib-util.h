3f2c028a1597eaf960cc649d36d87c27c7b7e39f
Dustin J. Mitchell <djmitche@users.sourceforge.net>
2007-09-25 03:10:48 +0000

    * device-src/device.c device-src/vfs-device.h   device-src/rait-device.h device-src/device.h   device-src/null-device.c device-src/tape-aix.c   device-src/tape-uware.c device-src/null-device.h   device-src/tape-xenix.c device-src/queueing.c   device-src/semaphore.c device-src/property.c   device-src/fd-device.c device-src/tape-device.c   device-src/queueing.h device-src/semaphore.h   device-src/property.h device-src/tape-posix.c   device-src/fd-device.h device-src/tape-device.h   device-src/vfs-device.c device-src/rait-device.c   device-src/tape-ops.h common-src/glib-util.c   common-src/glib-util.h: restrict to GPLv2
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@488 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/glib-util.h
+++ common-src/glib-util.h
@@ -1,27 +1,26 @@
 /*
- * Copyright (c) 2005 Zmanda Inc.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
+ * Copyright (c) 2005 Zmanda, Inc.  All Rights Reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1 as 
+ * published by the Free Software Foundation.
+ * 
+ * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- *
- * Contact information: Zmanda Inc, 505 N Mathlida Ave, Suite 120
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ * 
+ * Contact information: Zmanda Inc., 505 N Mathlida Ave, Suite 120
  * Sunnyvale, CA 94085, USA, or: http://www.zmanda.com
- *
- * Author: Dustin J. Mitchell <dustin@zmanda.com>, Ian Turner <ian@zmanda.com>
  */
 /*
  * Utilities that aren't quite included in glib
+ *
+ * Author: Dustin J. Mitchell <dustin@zmanda.com>, Ian Turner <ian@zmanda.com>
  */
 
 #ifndef GLIB_UTIL_H
--- device-src/null-device.h
+++ device-src/null-device.h
@@ -1,20 +1,21 @@
 /*
- * Amanda, The Advanced Maryland Automatic Network Disk Archiver
- * Copyright (c) 2005 Zmanda Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Copyright (c) 2005 Zmanda, Inc.  All Rights Reserved.
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1 as 
+ * published by the Free Software Foundation.
  * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ * 
+ * Contact information: Zmanda Inc., 505 N Mathlida Ave, Suite 120
+ * Sunnyvale, CA 94085, USA, or: http://www.zmanda.com
  */
 
 /* The NULL device accepts data and sends it to the bit bucket. Like
