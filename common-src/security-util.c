ecfd7eba520ca3212e64b1276355c5ab3842bc0f
Jean-Louis Martineau <martineau@zmanda.com>
2008-03-03 18:08:18 +0000

    Merged revisions 10391 via zmerge from svn+ssh://martineau@natasha/home/cvs/svn/amanda/trunk
    ........
      r10391 | martineau | 2008-03-03 13:06:11 -0500 (Mon, 03 Mar 2008) | 3 lines
      
      BugId: 4179
      * common-src/security-util.c: getnameinfo return != 0 on error.
    ........
    
    
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@845 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/security-util.c
+++ common-src/security-util.c
@@ -1380,7 +1380,7 @@ udp_netfd_read_callback(
 
     result = getnameinfo((struct sockaddr *)&udp->peer, SS_LEN(&udp->peer),
 			 hostname, sizeof(hostname), NULL, 0, 0);
-    if (result == -1) {
+    if (result != 0) {
 	dbprintf("getnameinfo failed: %s\n",
 		  gai_strerror(result));
 	security_seterror(&rh->sech, "getnameinfo failed: %s",
@@ -2292,7 +2292,7 @@ check_security(
 
     /* what host is making the request? */
     if ((result = getnameinfo((struct sockaddr *)addr, SS_LEN(addr),
-			      hostname, NI_MAXHOST, NULL, 0, 0)) == -1) {
+			      hostname, NI_MAXHOST, NULL, 0, 0)) != 0) {
 	dbprintf(_("getnameinfo failed: %s\n"),
 		  gai_strerror(result));
 	*errstr = vstralloc("[", "addr ", str_sockaddr(addr), ": ",
