2d963488ccb3ca930a92b842f0208d64e4a531ae
Nikolas Coukouma <atrus@zmanda.com>
2008-12-05 20:16:46 +0000

    * server-src/amrmtape.pl:   man/xml-source/amrmtape.8.xml,   installcheck/amrmtape.pl:  add --empty and --changer options for erasing media
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@1428 a8d146d6-cc15-0410-8900-af154a0219e0

--- installcheck/amrmtape.pl
+++ installcheck/amrmtape.pl
@@ -16,16 +16,18 @@
 # Contact information: Zmanda Inc, 465 S Mathlida Ave, Suite 300
 # Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
 
-use Test::More tests => 4;
+use Test::More tests => 16;
 
 use lib "@amperldir@";
 use Installcheck::Config;
+use Amanda::Device qw( :constants );
 use Installcheck::Run qw(run run_err $diskname);
 use Amanda::Config qw( :init config_dir_relative );
 use Amanda::Paths;
 use Amanda::Tapelist;
 
 my $testconf;
+my $dev;
 
 $testconf = Installcheck::Run::setup();
 $testconf->add_param('label_new_tapes', '"TESTCONF%%"');
@@ -44,6 +46,59 @@ ok(run('amrmtape', 'TESTCONF', 'TESTCONF01'), "amrmtape runs successfully")
 );
 
 $tapelist = Amanda::Tapelist::read_tapelist(config_dir_relative("tapelist"));
-ok(!$tapelist->lookup_tapelabel('TESTCONF01')) or diag("succesfully looked up tape that should have been removed");
+ok(!$tapelist->lookup_tapelabel('TESTCONF01')) or diag("should fail to look up tape that should has been removed");
+
+$dev = Amanda::Device->new('file:' . Installcheck::Run::vtape_dir());
+
+ok($dev->start($ACCESS_READ, undef, undef),
+    "start device in read mode")
+    or diag($dev->error_or_status());
+
+ok($dev->finish(),
+   "finish device after starting")
+    or diag($dev->error_or_status());
+
+# test --erase
+
+ok(run('amdump', 'TESTCONF'), "amdump ran successfully");
+
+ok(run('amrmtape', '--erase', 'TESTCONF', 'TESTCONF01'), "amrmtape runs successfully with --erase")
+    or diag(join("\n", 'stdout:', $Installcheck::Run::stdout, '', 'stderr:', $Installcheck::Run::stderr)
+);
+
+$tapelist = Amanda::Tapelist::read_tapelist(config_dir_relative("tapelist"));
+ok(!$tapelist->lookup_tapelabel('TESTCONF01')) or diag("succesfully looked up tape that should have been removed after --erase");
+
+$dev = Amanda::Device->new('file:' . Installcheck::Run::vtape_dir());
+
+ok(!$dev->start($ACCESS_READ, undef, undef),
+    "start device in read mode fails")
+    or diag($dev->error_or_status());
+
+# just in case the above does start the device
+ok($dev->finish(),
+   "finish device (just in case)")
+    or diag($dev->error_or_status());
+
+# test --dryrun and --erase
+
+ok(run('amdump', 'TESTCONF'), "amdump ran successfully");
+
+ok(run('amrmtape', '--dryrun', '--erase', 'TESTCONF', 'TESTCONF01'), "amrmtape runs successfully with --dryrun --erase")
+    or diag(join("\n", 'stdout:', $Installcheck::Run::stdout, '', 'stderr:', $Installcheck::Run::stderr)
+);
+
+$tapelist = Amanda::Tapelist::read_tapelist(config_dir_relative("tapelist"));
+ok($tapelist->lookup_tapelabel('TESTCONF01')) or diag("succesfully looked up tape that should NOT have been removed");
+
+$dev = Amanda::Device->new('file:' . Installcheck::Run::vtape_dir());
+
+ok($dev->start($ACCESS_READ, undef, undef),
+    "start device in read mode")
+    or diag($dev->error_or_status());
+
+ok($dev->finish(),
+   "finish device after starting")
+    or diag($dev->error_or_status());
 
 Installcheck::Run::cleanup();
--- server-src/amrmtape.pl
+++ server-src/amrmtape.pl
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA 
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
 # Contact information: Zmanda Inc, 465 S Mathlida Ave, Suite 300
 # Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
@@ -23,7 +23,10 @@ use strict;
 use warnings;
 
 use Amanda::Config qw( :init :getconf config_print_errors config_dir_relative );
+use Amanda::Changer;
+use Amanda::Device qw( :constants );
 use Amanda::Paths;
+use Amanda::MainLoop;
 use Amanda::Tapelist;
 use Amanda::Util qw( :constants );
 use File::Copy;
@@ -35,6 +38,8 @@ my $suf = ( $USE_VERSION_SUFFIXES =~ /^yes$/i ) ? '-@VERSION@' : '';
 my $amadmin = "$sbindir/amadmin$suf";
 
 my $dry_run;
+my $erase;
+my $changer_name;
 my $verbose = 1;
 my $help;
 
@@ -43,6 +48,8 @@ sub usage() {
 $0 [-n] [-v] [-q] [-d] <configuration> <label>
 \t--dryrun
 \t-n Do nothing to original files, leave new ones in database directory.
+\t--erase Erase the media, if possible
+\t--changer changer-name Specify the name of the changer to use (for --erase).
 \t--verbose
 \t-v Verbose, list backups of hosts and disks that are being discarded.
 \t--quiet
@@ -69,6 +76,8 @@ Amanda::Util::setup_application("amrmtape", "server", $CONTEXT_CMDLINE);
 
 my $opts_ok = GetOptions(
     "dryrun|n" => \$dry_run,
+    "erase" => \$erase,
+    "changer=s" => \$changer_name,
     "verbose|v" => \$verbose,
     "quiet|q" => sub {undef $verbose;},
     "help|h" => \$help,
@@ -100,88 +109,123 @@ if ($cfgerr_level >= $CFGERR_WARNINGS) {
 
 Amanda::Util::finish_setup($RUNNING_AS_DUMPUSER);
 
-my $tapelist_file = config_dir_relative(getconf($CNF_TAPELIST));
-my $tapelist = Amanda::Tapelist::read_tapelist($tapelist_file);
-unless ($tapelist) {
-    die "Could not read the tapelist";
-}
-
-$tapelist->remove_tapelabel($label);
-my $tmp_tapelist_file = "$AMANDA_TMPDIR/tapelist-amrmtape-" . time();
-my $backup_tapelist_file = "$AMANDA_TMPDIR/tapelist-backup-amrmtape-" . time();
-# writing to temp and then moving is generally safer than writing directly
-unless ($dry_run) {
-    unless (copy($tapelist_file, $backup_tapelist_file)) {
-        die "Failed to copy/backup $tapelist_file to $backup_tapelist_file";
+my $scrub_db = sub {
+    my $tapelist_file = config_dir_relative(getconf($CNF_TAPELIST));
+    my $tapelist = Amanda::Tapelist::read_tapelist($tapelist_file);
+    unless ($tapelist) {
+        die "Could not read the tapelist";
     }
-    $tapelist->write($tmp_tapelist_file);
-    unless (move($tmp_tapelist_file, $tapelist_file)) {
-        die "Failed to replace old tapelist  with new tapelist.";
-    }
-}
 
-my $tmp_curinfo_file = "$AMANDA_TMPDIR/tapelist-amrmtape-" . time();
-unless (open(AMADMIN, "$amadmin $config_name export |")) {
-    die "Failed to execute $amadmin: $! $?";
-}
-open(CURINFO, ">$tmp_curinfo_file");
-
-sub info_line($) {
-    print CURINFO "$_[0]";
-}
-
-my $host;
-my $disk;
-my $dead_level = 10;
-while(my $line = <AMADMIN>) {
-    my @parts = split(/\s+/, $line);
-    if ($parts[0] =~ /^CURINFO|#|(?:command|last_level|consecutive_runs|(?:full|incr)-(?:rate|comp)):$/) {
-        info_line $line;
-    } elsif ($parts[0] eq 'host:') {
-        $host = $parts[1];
-        info_line $line;
-    } elsif ($parts[0] eq 'disk:') {
-        $disk = $parts[1];
-        info_line $line;
-    } elsif ($parts[0] eq 'history:') {
-        info_line $line;
-    } elsif ($line eq "//\n") {
-        info_line $line;
-        $dead_level = 10;
-    } elsif ($parts[0] eq 'stats:') {
-        if (scalar(@parts) < 6 || scalar(@parts) > 8) {
-            die "unexpected number of fields in \"stats\" entry for $host:$disk\n\t$line";
+    $tapelist->remove_tapelabel($label);
+    my $tmp_tapelist_file = "$AMANDA_TMPDIR/tapelist-amrmtape-" . time();
+    my $backup_tapelist_file = "$AMANDA_TMPDIR/tapelist-backup-amrmtape-" . time();
+    # writing to temp and then moving is generally safer than writing directly
+    unless ($dry_run) {
+        unless (copy($tapelist_file, $backup_tapelist_file)) {
+            die "Failed to copy/backup $tapelist_file to $backup_tapelist_file";
         }
-	my $level = $parts[1];
-        my $cur_label = $parts[7];
-        if ($cur_label eq $label) {
-            $dead_level = $level;
-            vlog "Discarding Host: $host, Disk: $disk, Level: $level";
-	} elsif ( $level > $dead_level ) {
-            vlog "Discarding Host: $host, Disk: $disk, Level: $level";
-        } else {
+        $tapelist->write($tmp_tapelist_file);
+        unless (move($tmp_tapelist_file, $tapelist_file)) {
+            die "Failed to replace old tapelist  with new tapelist.";
+        }
+    }
+
+    my $tmp_curinfo_file = "$AMANDA_TMPDIR/curinfo-amrmtape-" . time();
+    unless (open(AMADMIN, "$amadmin $config_name export |")) {
+        die "Failed to execute $amadmin: $! $?";
+    }
+    open(CURINFO, ">$tmp_curinfo_file");
+
+    sub info_line($) {
+        print CURINFO "$_[0]";
+    }
+
+    my $host;
+    my $disk;
+    my $dead_level = 10;
+    while(my $line = <AMADMIN>) {
+        my @parts = split(/\s+/, $line);
+        if ($parts[0] =~ /^CURINFO|#|(?:command|last_level|consecutive_runs|(?:full|incr)-(?:rate|comp)):$/) {
             info_line $line;
+        } elsif ($parts[0] eq 'host:') {
+            $host = $parts[1];
+            info_line $line;
+        } elsif ($parts[0] eq 'disk:') {
+            $disk = $parts[1];
+            info_line $line;
+        } elsif ($parts[0] eq 'history:') {
+            info_line $line;
+        } elsif ($line eq "//\n") {
+            info_line $line;
+            $dead_level = 10;
+        } elsif ($parts[0] eq 'stats:') {
+            if (scalar(@parts) < 6 || scalar(@parts) > 8) {
+                die "unexpected number of fields in \"stats\" entry for $host:$disk\n\t$line";
+            }
+            my $level = $parts[1];
+            my $cur_label = $parts[7];
+            if ($cur_label eq $label) {
+                $dead_level = $level;
+                vlog "Discarding Host: $host, Disk: $disk, Level: $level\n";
+            } elsif ( $level > $dead_level ) {
+                vlog "Discarding Host: $host, Disk: $disk, Level: $level\n";
+            } else {
+                info_line $line;
+            }
+        } else {
+            die "Error: unrecognized line of input:\n\t$line";
         }
-    } else {
-        die "Error: unrecognized line of input:\n\t$line";
     }
-}
 
-close CURINFO;
-unless (close AMADMIN) {
-    unlink $tmp_curinfo_file;
-    die "$amadmin exited with non-zero while exporting: $! $?";
-}
-
-unless ($dry_run) {
-    if (system("$amadmin $config_name import < $tmp_curinfo_file")) {
-        unless (move($backup_tapelist_file, $tapelist_file)) {
-            printf STDERR "Failed to rollback new tapelist.\n";
-        }
+    close CURINFO;
+    unless (close AMADMIN) {
         unlink $tmp_curinfo_file;
-        die "$amadmin exited with non-zero while importing: $! $?";
+        die "$amadmin exited with non-zero while exporting: $! $?";
     }
-}
 
-unlink $tmp_curinfo_file;
-unlink $backup_tapelist_file;
+    unless ($dry_run) {
+        if (system("$amadmin $config_name import < $tmp_curinfo_file")) {
+            unless (move($backup_tapelist_file, $tapelist_file)) {
+                printf STDERR "Failed to rollback new tapelist.\n";
+            }
+            unlink $tmp_curinfo_file;
+            die "$amadmin exited with non-zero while importing: $! $?";
+        }
+    }
+
+    unlink $tmp_curinfo_file;
+    unlink $backup_tapelist_file;
+
+    Amanda::MainLoop::quit();
+};
+
+my $erase_volume = sub {
+    if ($erase) {
+	my $chg = Amanda::Changer->new($changer_name);
+	$chg->load(
+	    'label' => $label,
+	    'res_cb' => sub {
+		my ($err, $resv) = @_;
+		die $err if $err;
+
+		my $dev = Amanda::Device->new($resv->{device_name});
+                die "Can not erase $label because the device doesn't support this feature"
+                    unless $dev->property_get('full_deletion');
+		if (!$dry_run) {
+		    $dev->start($ACCESS_APPEND, undef, undef)
+			or die "Could not start device in append mode";
+		    $dev->erase()
+			or die "Failed to erase volume";
+		    $dev->finish();
+		}
+
+		$scrub_db->();
+	    });
+    } else {
+	$scrub_db->();
+    }
+};
+
+# kick things off
+Amanda::MainLoop::call_later($erase_volume);
+Amanda::MainLoop::run();
