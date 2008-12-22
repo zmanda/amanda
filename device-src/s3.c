d0712e729586238921e7725b704c86b5f009dd1f
Nikolas Coukouma <atrus@zmanda.com>
2008-12-22 21:22:19 +0000

    retry on CURLE_GOT_NOTHING (52) to handle "connection reset by peer"
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@1532 a8d146d6-cc15-0410-8900-af154a0219e0

--- device-src/s3.c
+++ device-src/s3.c
@@ -115,7 +115,8 @@
         { 0,    0,                           CURLE_PARTIAL_FILE,        S3_RESULT_RETRY }, \
         { 0,    0,                           CURLE_OPERATION_TIMEOUTED, S3_RESULT_RETRY }, \
         { 0,    0,                           CURLE_SEND_ERROR,          S3_RESULT_RETRY }, \
-        { 0,    0,                           CURLE_RECV_ERROR,          S3_RESULT_RETRY }
+        { 0,    0,                           CURLE_RECV_ERROR,          S3_RESULT_RETRY }, \
+        { 0,    0,                           CURLE_GOT_NOTHING,         S3_RESULT_RETRY }
 
 /*
  * Data structures and associated functions
