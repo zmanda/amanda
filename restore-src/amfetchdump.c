240bea48229536806f21e6462ba332576b0629b6
Jean-Louis Martineau <martineau@zmanda.com>
2009-01-08 20:30:24 +0000

    * restore-src/amfetchdump.c: Write to stderr.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@1549 a8d146d6-cc15-0410-8900-af154a0219e0

--- restore-src/amfetchdump.c
+++ restore-src/amfetchdump.c
@@ -146,7 +146,7 @@ list_needed_tapes(
     for(curmatch = matches; curmatch; curmatch = curmatch->next) {
 	int havetape = 0;
 
-	printf("Examining %s %s on %s\n", curmatch->hostname, curmatch->diskname, curmatch->label);
+	g_fprintf(stderr, "Examining %s %s on %s\n", curmatch->hostname, curmatch->diskname, curmatch->label);
 	/* keep only first dump if only_one */
 	if (only_one &&
 	    curmatch != matches &&
@@ -154,7 +154,7 @@ list_needed_tapes(
 	     strcmp(curmatch->diskname, matches->diskname) ||
 	     strcmp(curmatch->timestamp, matches->timestamp) ||
 	     curmatch->level != matches->level)) {
-	    printf("only_one matched\n");
+	    g_fprintf(stderr, "only_one matched\n");
 	    continue;
 	}
 	if(strcmp("OK", curmatch->status)){
@@ -162,7 +162,7 @@ list_needed_tapes(
 		             curmatch->timestamp, curmatch->hostname,
 			     curmatch->diskname, curmatch->level,
 			     curmatch->status);
-	    printf("!OK\n");
+	    g_fprintf(stderr, "!OK\n");
 	    continue;
 	}
 
