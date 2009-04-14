67c47bb2217941a57c64380584e641efd21e1ded
Jean-Louis Martineau <martineau@zmanda.com>
2009-04-14 22:07:05 +0000

    * amandad-src/amandad.c: Improve error message.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@1866 a8d146d6-cc15-0410-8900-af154a0219e0

--- amandad-src/amandad.c
+++ amandad-src/amandad.c
@@ -429,12 +429,12 @@ main(
 
     if (getuid() == 0) {
 	if (strcasecmp(auth, "krb5") != 0) {
-	    error(_("Amanda must be run as user '%s' when using '%s' authentication"),
+	    error(_("'amandad' must be run as user '%s' when using '%s' authentication"),
 		  CLIENT_LOGIN, auth);
 	}
     } else {
 	if (strcasecmp(auth, "krb5") == 0) {
-	    error(_("Amanda must be run as user 'root' when using 'krb5' authentication"));
+	    error(_("'amandad' must be run as user 'root' when using 'krb5' authentication"));
 	}
     }
 
