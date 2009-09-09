da5af76c362a5509f359eaecbb5f3a850c6e7fb1
Dustin J. Mitchell <djmitche@users.sourceforge.net>
2009-09-09 16:02:02 +0000

    * installcheck/Amanda_Device.pl: fix skip count
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@2139 a8d146d6-cc15-0410-8900-af154a0219e0

--- installcheck/Amanda_Device.pl
+++ installcheck/Amanda_Device.pl
@@ -573,7 +573,7 @@ my $base_name;
 
 SKIP: {
     skip "define \$INSTALLCHECK_S3_{SECRET,ACCESS}_KEY to run S3 tests",
-            64 +
+            67 +
             1 * $verify_file_count +
             4 * $write_file_count +
             10 * $s3_make_device_count
