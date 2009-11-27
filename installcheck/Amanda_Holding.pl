31dc28d757f1a928c0965870a1e9b67f03f6c99c
Dustin J. Mitchell <djmitche@users.sourceforge.net>
2009-11-27 15:18:37 +0000

    * installcheck/Amanda_Holding.pl installcheck/amrestore.pl:   use unary + before the arguments to sort, since perl-5.6's   parser gets confused otherwise
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@2281 a8d146d6-cc15-0410-8900-af154a0219e0

--- installcheck/Amanda_Holding.pl
+++ installcheck/Amanda_Holding.pl
@@ -125,7 +125,7 @@ is_deeply([ sort(Amanda::Holding::disks()) ],
     [ sort($holding1, $holding2) ],
     "all active holding disks, but not inactive (defined but not used) disks");
 
-is_deeply([ sort(Amanda::Holding::files()) ],
+is_deeply([ sort(+Amanda::Holding::files()) ],
     [ sort(
 	"$holding1/20070303000000/videoserver._video_a",
 	"$holding1/20070306123456/videoserver._video_a",
@@ -135,7 +135,7 @@ is_deeply([ sort(Amanda::Holding::files()) ],
     ) ],
     "all files");
 
-is_deeply([ sort(Amanda::Holding::file_chunks("$holding2/20070306123456/audio._var")) ],
+is_deeply([ sort(+Amanda::Holding::file_chunks("$holding2/20070306123456/audio._var")) ],
     [ sort(
 	"$holding2/20070306123456/audio._var",
 	"$holding2/20070306123456/audio._var.1",
@@ -161,7 +161,7 @@ is_deeply([ Amanda::Holding::get_all_datestamps() ],
 	  [ sort("20070303000000", "20070306123456") ],
 	  "get_all_datestamps");
 
-is_deeply([ sort(Amanda::Holding::get_files_for_flush("023985")) ],
+is_deeply([ sort(+Amanda::Holding::get_files_for_flush("023985")) ],
 	  [ sort() ],
 	  "get_files_for_flush with no matching datestamps returns no files");
 is_deeply([ Amanda::Holding::get_files_for_flush("20070306123456") ],
