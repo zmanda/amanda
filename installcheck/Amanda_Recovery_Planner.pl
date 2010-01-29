3868ffd7bebd7d8914379975322bceb82a2528e0
Dustin J. Mitchell <djmitche@users.sourceforge.net>
2010-01-29 18:16:09 +0000

    * perl/Makefile.am perl/Amanda/Recovery/Planner.pm: new package * installcheck/Amanda_Recovery_Planner.pl   installcheck/Makefile.am: tests
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@2480 a8d146d6-cc15-0410-8900-af154a0219e0

---
+++ installcheck/Amanda_Recovery_Planner.pl
@@ -0,0 +1,386 @@
+# Copyright (c) 2010 Zmanda Inc.  All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# Contact information: Zmanda Inc, 465 S. Mathilda Ave., Suite 300
+# Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
+
+use Test::More tests => 5;
+use File::Path;
+use Data::Dumper;
+use strict;
+use warnings;
+
+use lib "@amperldir@";
+use Installcheck::Config;
+use Amanda::Config qw( :init :getconf config_dir_relative );
+use Amanda::Changer;
+use Amanda::Debug;
+use Amanda::DB::Catalog;
+use Amanda::Recovery::Planner;
+use Amanda::MainLoop;
+
+# and disable Debug's die() and warn() overrides
+Amanda::Debug::disable_die_override();
+
+# put the debug messages somewhere
+Amanda::Debug::dbopen("installcheck");
+Installcheck::log_test_output();
+
+my $testconf;
+$testconf = Installcheck::Config->new();
+$testconf->write();
+
+my $cfg_result = config_init($CONFIG_INIT_EXPLICIT_NAME, 'TESTCONF');
+if ($cfg_result != $CFGERR_OK) {
+    my ($level, @errors) = Amanda::Config::config_errors();
+    die(join "\n", @errors);
+}
+
+##
+# Fill in some fake logfiles
+
+my $logdir = config_dir_relative(getconf($CNF_LOGDIR));
+my $tapelist_fn = config_dir_relative(getconf($CNF_TAPELIST));
+my $write_timestamp;
+my $output;
+open (my $tapelist, ">", $tapelist_fn);
+while (<DATA>) {
+    # skip comments
+    next if (/^#/ or /^\S*$/);
+
+    # add to tapelist
+    if (/^:tapelist (\d+) (\S+)\s*$/) {
+	print $tapelist "$1 $2 reuse\n";
+	next;
+    }
+
+    # new logfile
+    if (/^::: (.*)/) {
+	open $output, ">", "$logdir/$1" or die("Could not open $1 for writing: $!");
+	next;
+    }
+
+    die("syntax error") if (/^:/);
+
+    print $output $_;
+}
+close($output);
+close($tapelist);
+
+##
+## Tests!
+###
+
+sub make_plan_sync {
+    my $plan;
+
+    Amanda::Recovery::Planner::make_plan(@_,
+	debug => 1,
+	plan_cb => sub {
+	    (my $err, $plan) = @_;
+	    die "$err" if $err;
+	    Amanda::MainLoop::quit();
+	});
+
+    Amanda::MainLoop::run();
+    return $plan;
+}
+
+sub ds {
+    return Amanda::Cmdline::dumpspec_t->new($_[0], $_[1], $_[2], $_[3]);
+}
+
+sub is_plan {
+    my ($got, $exp, $msg) = @_;
+    my $got_dumps = $got->{'dumps'};
+
+    # make an "abbreviated" version of the plan for comparison with the
+    # expected
+    my @got_abbrev;
+    for my $d (@$got_dumps) {
+	my @parts;
+	push @got_abbrev, [
+	    $d->{'hostname'},
+	    $d->{'diskname'},
+	    $d->{'dump_timestamp'},
+	    $d->{'level'},
+	    \@parts ];
+
+	for my $p (@{$d->{'parts'}}) {
+	    next unless defined $p;
+	    push @parts,
+		$p->{'label'},
+		"$p->{filenum}"+0; # strip bigints
+	}
+    }
+
+    is_deeply(\@got_abbrev, $exp, $msg)
+	or diag("got:\n" . Dumper(\@got_abbrev));
+}
+
+my $changer = undef; # not needed yet
+
+is_plan(make_plan_sync(
+	    dumpspec => ds("no-box-at-all"),
+	    changer => $changer),
+    [ ],
+    "empty plan for nonexistent host");
+
+is_plan(make_plan_sync(
+	    dumpspec => ds("somebox", "^/lib", "200801"),
+	    changer => $changer),
+    [
+	[   "somebox", "/lib", "20080111000000", 0, [
+		'Conf-001' => 1,
+	    ],
+	],
+    ],
+    "simple plan for just one dump");
+
+is_plan(make_plan_sync(
+	    dumpspec => ds("somebox", "^/lib"),
+	    changer => $changer),
+    [
+	[   "somebox", "/lib", "20080111000000", 0, [
+		'Conf-001' => 1,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 2,
+		'Conf-003' => 3,
+		'Conf-003' => 4,
+		'Conf-003' => 5,
+		'Conf-003' => 6,
+		'Conf-003' => 7,
+		'Conf-003' => 8,
+		'Conf-003' => 9,
+		'Conf-003' => 10,
+		'Conf-003' => 11,
+	    ],
+	],
+    ],
+    "plan for two dumps, in order by tape write time");
+
+is_plan(make_plan_sync(
+	    dumpspecs => [
+		ds("somebox", "^/lib", "20080111"),
+		ds("euclid", "/home/dustin/code/backuppc"),
+	    ],
+	    changer => $changer),
+    [
+	[   "somebox", "/lib", "20080111000000", 0, [
+		'Conf-001' => 1,
+	    ],
+	],
+	[   "euclid", "/home/dustin/code/backuppc", "20100127172011", 0, [
+		'Conf-013' => 1,
+		'Conf-013' => 2,
+		'Conf-013' => 3,
+		'Conf-014' => 1,
+		'Conf-014' => 2,
+		'Conf-014' => 3,
+	    ],
+	],
+    ],
+    "plan for two dumps, one of them spanned, in order by tape write time");
+
+is_plan(make_plan_sync(
+	    dumpspec => ds("somebox", "^/lib", "200803"),
+	    one_dump_per_part => 1,
+	    changer => $changer),
+    [
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 2,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 3,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 4,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 5,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 6,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 7,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 8,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 9,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 10,
+	    ],
+	],
+	[   "somebox", "/lib", "20080313133333", 0, [
+		'Conf-003' => 11,
+	    ],
+	],
+    ],
+    "plan for a multipart dump, one_dump_per_part");
+
+
+__DATA__
+# a short-datestamp logfile with only a single, single-part file in it
+::: log.20080111.0
+:tapelist 20080111 Conf-001
+DISK planner somebox /lib
+START planner date 20080111
+START driver date 20080111
+STATS driver hostname somebox
+STATS driver startup time 0.051
+FINISH planner date 20080111 time 82.721
+START taper datestamp 20080111 label Conf-001 tape 1
+SUCCESS dumper somebox /lib 20080111 0 [sec 0.209 kb 1970 kps 9382.2 orig-kb 1970]
+SUCCESS chunker somebox /lib 20080111 0 [sec 0.305 kb 420 kps 1478.7]
+STATS driver estimate somebox /lib 20080111 0 [sec 1 nkb 2002 ckb 480 kps 385]
+PART taper Conf-001 1 somebox /lib 20080111 1/1 0 [sec 4.813543 kb 419 kps 87.133307]
+DONE taper somebox /lib 20080111 1 0 [sec 4.813543 kb 419 kps 87.133307]
+FINISH driver date 20080111 time 2167.581
+
+# a logfile with several dumps in it, one of which comes in many parts, and one of which is
+# from a previous run
+::: log.20080313133333.0
+:tapelist 20080313133333 Conf-003
+DISK planner somebox /usr/bin
+DISK planner somebox /lib
+DISK planner otherbox /lib
+DISK planner otherbox /usr/bin
+START planner date 20080313133333
+START driver date 20080313133333
+STATS driver hostname somebox
+STATS driver startup time 0.059
+INFO planner Full dump of somebox:/lib promoted from 2 days ahead.
+FINISH planner date 20080313133333 time 0.286
+SUCCESS dumper somebox /usr/bin 20080313133333 1 [sec 0.001 kb 20 kps 10352.0 orig-kb 20]
+SUCCESS chunker somebox /usr/bin 20080313133333 1 [sec 1.023 kb 20 kps 50.8]
+STATS driver estimate somebox /usr/bin 20080313133333 1 [sec 0 nkb 52 ckb 64 kps 1024]
+START taper datestamp 20080313133333 label Conf-003 tape 1
+PART taper Conf-003 1 somebox /usr/bin 20080313133333 1/1 1 [sec 0.000370 kb 20 kps 54054.054054]
+DONE taper somebox /usr/bin 20080313133333 1 1 [sec 0.000370 kb 20 kps 54054.054054]
+# a multi-part dump
+SUCCESS dumper somebox /lib 20080313133333 0 [sec 0.189 kb 3156 kps 50253.1 orig-kb 3156]
+SUCCESS chunker somebox /lib 20080313133333 0 [sec 5.250 kb 3156 kps 1815.5]
+STATS driver estimate somebox /lib 20080313133333 0 [sec 1 nkb 3156 ckb 3156 kps 9500]
+PART taper Conf-003 2 somebox /lib 20080313133333 1/10 0 [sec 0.005621 kb 1024 kps 182173.990393]
+PART taper Conf-003 3 somebox /lib 20080313133333 2/10 0 [sec 0.006527 kb 1024 kps 156886.777999]
+PART taper Conf-003 4 somebox /lib 20080313133333 3/10 0 [sec 0.005854 kb 1024 kps 174923.129484]
+PART taper Conf-003 5 somebox /lib 20080313133333 4/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 6 somebox /lib 20080313133333 5/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 7 somebox /lib 20080313133333 6/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 8 somebox /lib 20080313133333 7/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 9 somebox /lib 20080313133333 8/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 10 somebox /lib 20080313133333 9/10 0 [sec 0.007344 kb 1024 kps 147993.746743]
+PART taper Conf-003 11 somebox /lib 20080313133333 10/10 0 [sec 0.001919 kb 284 kps 147993.746743]
+DONE taper somebox /lib 20080313133333 10 0 [sec 0.051436 kb 3156 kps 184695.543977]
+SUCCESS dumper otherbox /lib 20080313133333 0 [sec 0.001 kb 190 kps 10352.0 orig-kb 20]
+SUCCESS chunker otherbox /lib 20080313133333 0 [sec 1.023 kb 190 kps 50.8]
+STATS driver estimate otherbox /lib 20080313133333 0 [sec 0 nkb 190 ckb 190 kps 1024]
+# this dump is from a previous run, with an older dump_timestamp
+PART taper Conf-003 12 otherbox /usr/bin 20080311131133 1/1 0 [sec 0.002733 kb 240 kps 136425.648022]
+DONE taper otherbox /usr/bin 20080311131133 1 0 [sec 0.002733 kb 240 kps 136425.648022]
+PART taper Conf-003 13 otherbox /lib 20080313133333 1/1 0 [sec 0.001733 kb 190 kps 136425.648022]
+DONE taper otherbox /lib 20080313133333 1 0 [sec 0.001733 kb 190 kps 136425.648022]
+FINISH driver date 20080313133333 time 24.777
+
+# A logfile with some partial parts (PARTPARTIAL) in it
+::: log.20080414144444.0
+:tapelist 20080414144444 Conf-004
+:tapelist 20080414144444 Conf-005
+DISK planner otherbox /lib
+START planner date 20080414144444
+START driver date 20080414144444
+STATS driver hostname otherbox
+STATS driver startup time 0.075
+INFO taper Will write new label `Conf-004' to new (previously non-amanda) tape
+FINISH planner date 20080414144444 time 2.139
+SUCCESS dumper otherbox /lib 20080414144444 1 [sec 0.003 kb 60 kps 16304.3 orig-kb 60]
+SUCCESS chunker otherbox /lib 20080414144444 1 [sec 1.038 kb 60 kps 88.5]
+STATS driver estimate otherbox /lib 20080414144444 1 [sec 0 nkb 92 ckb 96 kps 1024]
+START taper datestamp 20080414144444 label Conf-004 tape 1
+PARTPARTIAL taper Conf-004 1 otherbox /lib 20080414144444 1/1 1 [sec 0.000707 kb 32 kps 45261.669024] ""
+INFO taper Will request retry of failed split part.
+INFO taper Will write new label `Conf-005' to new (previously non-amanda) tape
+START taper datestamp 20080414144444 label Conf-005 tape 2
+PARTPARTIAL taper Conf-005 1 otherbox /lib 20080414144444 1/1 1 [sec 0.000540 kb 32 kps 59259.259259] ""
+INFO taper Will request retry of failed split part.
+WARNING driver Out of tapes; going into degraded mode.
+PARTIAL taper otherbox /lib 20080414144444 1 1 [sec 0.000540 kb 32 kps 59259.259259] "full-up"
+# a completely failed dump
+FAIL taper otherbox /boot 20080414144444 0 "no-space"
+FINISH driver date 20080414144444 time 6.959
+
+# a spanned dump (yep, a real dump)
+::: log.20100127172011.0
+:tapelist 20100127172011 Conf-013
+:tapelist 20100127172011 Conf-014
+INFO amdump amdump pid 30186
+INFO planner planner pid 30207
+START planner date 20100127172011
+DISK planner euclid /home/dustin/code/backuppc
+INFO planner Adding new disk euclid:/home/dustin/code/backuppc.
+INFO driver driver pid 30208
+START driver date 20100127172011
+STATS driver hostname euclid
+INFO dumper dumper pid 30220
+STATS driver startup time 0.097
+INFO dumper dumper pid 30222
+INFO dumper dumper pid 30221
+INFO dumper dumper pid 30213
+INFO taper taper pid 30210
+FINISH planner date 20100127172011 time 1.224
+INFO planner pid-done 30207
+INFO taper Will write new label `Conf-013' to new tape
+INFO chunker chunker pid 30255
+INFO dumper gzip pid 30259
+SUCCESS dumper euclid /home/dustin/code/backuppc 20100127172011 0 [sec 0.933 kb 2770 kps 2968.5 orig-kb 2770]
+SUCCESS chunker euclid /home/dustin/code/backuppc 20100127172011 0 [sec 0.943 kb 2770 kps 2970.1]
+INFO chunker pid-done 30255
+STATS driver estimate euclid /home/dustin/code/backuppc 20100127172011 0 [sec 2 nkb 2802 ckb 2816 kps 1024]
+INFO dumper pid-done 30259
+START taper datestamp 20100127172011 label Conf-013 tape 1
+PART taper Conf-013 1 euclid /home/dustin/code/backuppc 20100127172011 1/-1 0 [sec 0.000763 kb 512 kps 670972.950092]
+PART taper Conf-013 2 euclid /home/dustin/code/backuppc 20100127172011 2/-1 0 [sec 0.000770 kb 512 kps 664770.167400]
+PART taper Conf-013 3 euclid /home/dustin/code/backuppc 20100127172011 3/-1 0 [sec 0.000877 kb 512 kps 583952.261903]
+PARTPARTIAL taper Conf-013 4 euclid /home/dustin/code/backuppc 20100127172011 4/-1 0 [sec 0.000689 kb 352 kps 510888.307044] "No space left on device"
+INFO taper Will request retry of failed split part.
+INFO taper tape Conf-013 kb 1536 fm 4 [OK]
+INFO taper Will write new label `Conf-014' to new tape
+START taper datestamp 20100127172011 label Conf-014 tape 2
+PART taper Conf-014 1 euclid /home/dustin/code/backuppc 20100127172011 4/-1 0 [sec 0.001346 kb 512 kps 380377.004130]
+PART taper Conf-014 2 euclid /home/dustin/code/backuppc 20100127172011 5/-1 0 [sec 0.001338 kb 512 kps 382524.888399]
+PART taper Conf-014 3 euclid /home/dustin/code/backuppc 20100127172011 6/-1 0 [sec 0.000572 kb 210 kps 367336.443449]
+DONE taper euclid /home/dustin/code/backuppc 20100127172011 6 0 [sec 0.005666 kb 2770 kps 488860.596548]
+INFO dumper pid-done 30213
+INFO dumper pid-done 30220
+INFO dumper pid-done 30222
+INFO taper tape Conf-014 kb 1234 fm 3 [OK]
+INFO dumper pid-done 30221
+INFO taper pid-done 30210
+FINISH driver date 20100127172011 time 4.197
+INFO driver pid-done 30208
---
+++ perl/Amanda/Recovery/Planner.pm
@@ -0,0 +1,328 @@
+# Copyright (c) 2010 Zmanda, Inc.  All Rights Reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 2.1 as
+# published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+#
+# Contact information: Zmanda Inc., 465 S. Mathilda Ave., Suite 300
+# Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
+
+=head1 NAME
+
+Amanda::Recovery::Planner - use the catalog to plan recoveries
+
+=head1 SYNOPSIS
+
+    my $plan;
+
+    $subs{'make_plan'} = make_cb(make_plan => sub {
+	Amanda::Recovery::Planner::make_plan(
+	    dumpspecs => [ $ds1, $ds2 ],
+	    algorithm => $algo,
+	    changer => $changer,
+	    plan_cb => $subs{'plan_cb'});
+    };
+
+    $subs{'plan_cb'} = make_cb(plan_cb => sub {
+	my ($err, $pl) = @_;
+	die $err if $err;
+
+	$plan = $pl;
+	$subs{'start_next_dumpfile'}->();
+    });
+
+    $subs{'start_next_dumpfile'} = make_cb(start_next_dumpfile => sub {
+	my $dump = $plan->shift_dump();
+	if (!$dump) {
+	    # .. all done!
+	}
+
+	print "recovering ", $dump->{'hostname'}, " ", $dump->{'diskname'}, "\n";
+	$clerk->get_xfer_src( .. dump => $dump .. );
+	# ..
+    });
+
+=head1 OVERVIEW
+
+This package determines the optimal way to recover dump files from storage.
+Its function is superficially fairly simple: given a collection of desired
+dumpfiles, it returns a Plan to recover those dumpfiles, specifying exactly the
+volumes and files that are needed, and the order in which they should be
+accesed.
+
+=head2 ALGORITHMS
+
+Several algorithms will soon be available for selecting volumes when a dumpfile
+appears in several places (e.g., from an amvault operation).  At the moment,
+the algorithm argument should be omitted, as this will eventually indicate that
+the user-configured algorithm should be applied.
+
+=head2 INSTANTIATING A PLAN
+
+Call C<make_plan> with the desired dumpspecs, a changer, and a callback:
+
+    Amanda::Recovery::Planner::make_plan(
+	dumpspecs => [ $ds1, $ds2, .. ],
+	changer => $chg,
+	plan_cb => $plan_cb);
+
+As a shortcut, you may also specify a single dumpspec:
+
+    Amanda::Recovery::Planner::make_plan(
+	dumpspec => $ds,
+	changer => $chg,
+	plan_cb => $plan_cb);
+
+Note that in this case, the resulting plan may contain more than one dump, if
+the dumpspec was not unambiguous.
+
+To select the planner algorithm, pass an C<algorithm> argument.  This argument
+is currently ignored and should be omitted.  If the optional argument C<debug>
+is given with a true value, then the Planner will log additional debug
+information to the Amanda debug logs.
+
+The optional argument C<one_dump_per_part> will create a "no-reassembly" plan,
+where each part appears as the only part in a unique dump.  The dump objects
+will have the key C<single_part> set to 1.
+
+The C<plan_cb> is called with two arguments:
+
+    $plan_cb->($err, $plan);
+
+If C<$err> is defined, it describes an error that occurred; otherwise, C<$plan>
+is the generated plan, as described below.
+
+Some algorithms may consult the changer's inventory to determine what volumes
+are available.  It is because of this asynchronous operation that C<make_plan>
+takes a callback instead of simply returning the plan.
+
+=head2 PLANS
+
+A Plan is a perl object describing the process for recovering zero or more
+dumpfiles.  Its principal components are dumps, in order, that are to be
+recovered, but the object presents some other interfaces that return useful
+information about the plan.
+
+The C<'dumps'> key holds the list of dumps, in the order they should be
+performed.  Callers should shift dumps off this list to present to the Clerk.
+
+To get a list of volumes that the plan requires, in order, use
+C<get_volume_list>.  Each volume is represented as a hash:
+
+  { label => 'DATA182', available => 1 }
+
+where C<available> is false if the planner did not find this volume in the
+changer.  Planners which do not consult the changer will have a false value for
+C<available>.
+
+=cut
+
+package Amanda::Recovery::Planner;
+
+use strict;
+use warnings;
+use Carp;
+
+sub make_plan {
+    my %params = @_;
+
+    $params{'dumpspecs'} = [ $params{'dumpspec'} ]
+	if exists $params{'dumpspec'};
+
+    for my $rq_param qw(changer plan_cb dumpspecs) {
+	croak "required parameter '$rq_param' mising"
+	    unless exists $params{$rq_param};
+    }
+
+    my $plan = Amanda::Recovery::Planner::Plan->new({
+	algo => $params{'algorithm'},
+	chg => $params{'changer'},
+	debug => $params{'debug'},
+	one_dump_per_part => $params{'one_dump_per_part'},
+    });
+
+    $plan->make_plan($params{'dumpspecs'}, $params{'plan_cb'});
+}
+
+package Amanda::Recovery::Planner::Plan;
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Amanda::Device qw( :constants );
+use Amanda::Header;
+use Amanda::Config qw( :getconf config_dir_relative );
+use Amanda::Debug qw( :logging );
+use Amanda::MainLoop;
+use Amanda::DB::Catalog;
+use Amanda::Tapelist;
+
+sub new {
+    my $class = shift;
+    my $self = shift;
+    return bless($self, $class);
+}
+
+sub make_plan {
+    my $self = shift;
+    my ($dumpspecs, $plan_cb) = @_;
+
+    # first, get the set of dumps that match these dumpspecs
+    my @dumps = Amanda::DB::Catalog::get_dumps(dumpspecs => $dumpspecs);
+
+    # now "bin" those by host/disk/dump_ts/level
+    my %dumps;
+    for my $dump (@dumps) {
+	my $k = join("\0", $dump->{'hostname'}, $dump->{'diskname'},
+			   $dump->{'dump_timestamp'}, $dump->{'level'});
+	$dumps{$k} = [] unless exists $dumps{$k};
+	push @{$dumps{$k}}, $dump;
+    }
+
+    # now select the "best" of each set of dumps, and put that in @dumps
+    @dumps = ();
+    for my $options (values %dumps) {
+	my @options = @$options;
+	# if there's only one option, the choice is easy
+	if (@options == 1) {
+	    push @dumps, $options[0];
+	    next;
+	}
+
+	# if there are several, narrow to those with an OK status or barring that,
+	# those with a PARTIAL status.  FAIL need not apply.
+	my @ok_options = grep { $_->{'status'} eq 'OK' } @options;
+	my @partial_options = grep { $_->{'status'} eq 'PARTIAL' } @options;
+
+	if (@ok_options) {
+	    @options = @ok_options;
+	} else {
+	    @options = @partial_options;
+	}
+
+	# now, take the one written longest ago - this gets us the dump on secondary
+	# media if it hasn't been overwritten, otherwise the dump on tertiary media,
+	# etc.
+	@options = Amanda::DB::Catalog::sort_dumps(['write_timestamp'], @options);
+	push @dumps, $options[0];
+    }
+
+    # at this point we have exactly one instance of each dump in @dumps.
+
+    # If one_dump_per_part was specified, rearrange @dumps to have a distinct
+    # dump object for each part.
+    if ($self->{'one_dump_per_part'}) {
+	@dumps = $self->split_dumps_per_part(\@dumps);
+    }
+
+    # now sort the dumps in order by their constituent parts.  This sorts based
+    # on write_timestamp, then on the label of the first part of the dump,
+    # using the tapelist to order the labels.  Where labels match, it sorts on
+    # the part's filenum.  This should sort the dumps into the order in which
+    # they were written.
+    my $tapelist_filename = config_dir_relative(getconf($CNF_TAPELIST));
+    my $tapelist = Amanda::Tapelist::read_tapelist($tapelist_filename);
+
+    my $sortfn = sub {
+	my $rv;
+	my $tle;
+
+	return $rv
+	    if ($rv = $a->{'write_timestamp'} cmp $b->{'write_timestamp'});
+
+	my ($alabel, $blabel) = (
+	    $a->{'parts'}[1]{'label'},
+	    $b->{'parts'}[1]{'label'},
+	);
+
+	my ($apos, $bpos);
+	$apos = $tle->{'position'}
+	    if (($tle = $tapelist->lookup_tapelabel($alabel)));
+	$bpos = $tle->{'position'}
+	    if (($tle = $tapelist->lookup_tapelabel($blabel)));
+	return ($bpos <=> $apos) # not: reversed for "oldest to newest"
+	    if defined $bpos && defined $apos && ($bpos <=> $apos);
+
+	# if a tape wasn't in the tapelist, just sort the labels lexically (this
+	# really shouldn't happen)
+	if (!defined $bpos || !defined $apos) {
+	    return $alabel cmp $blabel
+		if defined $alabel and defined $blabel and $alabel cmp $blabel ;
+	}
+
+	# finally, the dumps are on the same volume, so just sort by filenum
+	return $a->{'parts'}[1]{'filenum'} <=> $b->{'parts'}[1]{'filenum'};
+    };
+    @dumps = sort $sortfn @dumps;
+
+    $self->{'dumps'} = \@dumps;
+
+    Amanda::MainLoop::call_later($plan_cb, undef, $self);
+}
+
+sub split_dumps_per_part {
+    my $self = shift;
+    my ($dumps) = @_;
+
+    my @new_dumps;
+
+    for my $dump (@$dumps) {
+	for my $part (@{$dump->{'parts'}}) {
+	    my ($newdump, $newpart);
+
+	    # skip part 0
+	    next unless defined $part;
+
+	    # shallow copy the dump and part objects
+	    $newdump = do { my %t = %$dump; \%t; };
+	    $newpart = do { my %t = %$part; \%t; };
+
+	    # overwrite the interlinking
+	    $newpart->{'dump'} = $newdump;
+	    $newdump->{'parts'} = [ undef, $newpart ];
+
+	    $newdump->{'single_part'} = 1;
+
+	    push @new_dumps, $newdump;
+	}
+    }
+
+    return @new_dumps;
+}
+
+sub get_volume_list {
+    my $self = shift;
+    my $last_label;
+    my @volumes;
+
+    for my $dump (@{$self->{'dumps'}}) {
+	for my $part (@{$dump->{'parts'}}) {
+	    next unless defined $part; # skip parts[0]
+	    if (!defined $last_label || $part->{'label'} ne $last_label) {
+		$last_label = $part->{'label'};
+		push @volumes, { label => $last_label, available => 0 };
+	    }
+	}
+    }
+
+    return @volumes;
+}
+
+sub dbg {
+    my ($self, $msg) = @_;
+    if ($self->{'debug'}) {
+	debug("Amanda::Recovery::Planner: $msg");
+    }
+}
+
+1;
