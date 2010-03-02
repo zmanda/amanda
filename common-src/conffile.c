c564b9b6ba99cffda2423e250b170542f53f965c
Jean-Louis Martineau <martineau@zmanda.com>
2010-03-02 23:16:34 +0000

    * common-src/conffile.c: Correctly set autolabel from command line                          '-olabel_new_tapes=' options.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@2596 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -4797,11 +4797,18 @@ update_derived_values(
 	     getconf_seen(CNF_AUTOLABEL) < 0)) {
 		conf_parserror(_("Can't defined both label_new_tapes and autolabel"));
 	}
-	if (getconf_seen(CNF_LABEL_NEW_TAPES) != 0 &&
-	    getconf_seen(CNF_AUTOLABEL) == 0) {
+	if ((getconf_seen(CNF_LABEL_NEW_TAPES) != 0 &&
+	     getconf_seen(CNF_AUTOLABEL) == 0) ||
+	    (getconf_seen(CNF_LABEL_NEW_TAPES) < 0 &&
+	     getconf_seen(CNF_AUTOLABEL) >= 0)) {
 	    autolabel_t *autolabel = &(conf_data[CNF_AUTOLABEL].v.autolabel);
 	    autolabel->template = getconf_str(CNF_LABEL_NEW_TAPES);
-	    autolabel->autolabel = AL_VOLUME_ERROR | AL_EMPTY;
+	    if (!autolabel->template || autolabel->template == '\0') {
+		autolabel->template = NULL;
+		autolabel->autolabel = 0;
+	    } else {
+		autolabel->autolabel = AL_VOLUME_ERROR | AL_EMPTY;
+	    }
 	}
     }
 
