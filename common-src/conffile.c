a684bf8903d2cf2cbc6e5ea51f17d72354f8a822
Dustin J. Mitchell <djmitche@users.sourceforge.net>
2010-04-13 21:57:28 +0000

    * common-src/conffile.c: fix derived value for autolabel
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/branches/3_1@2878 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -4796,14 +4796,14 @@ update_derived_values(
 	     getconf_seen(CNF_AUTOLABEL) > 0)  ||
 	    (getconf_seen(CNF_LABEL_NEW_TAPES) < 0 &&
 	     getconf_seen(CNF_AUTOLABEL) < 0)) {
-		conf_parserror(_("Can't defined both label_new_tapes and autolabel"));
+		conf_parserror(_("Can't specify both label_new_tapes and autolabel"));
 	}
 	if ((getconf_seen(CNF_LABEL_NEW_TAPES) != 0 &&
 	     getconf_seen(CNF_AUTOLABEL) == 0) ||
 	    (getconf_seen(CNF_LABEL_NEW_TAPES) < 0 &&
 	     getconf_seen(CNF_AUTOLABEL) >= 0)) {
 	    autolabel_t *autolabel = &(conf_data[CNF_AUTOLABEL].v.autolabel);
-	    autolabel->template = getconf_str(CNF_LABEL_NEW_TAPES);
+	    autolabel->template = g_strdup(getconf_str(CNF_LABEL_NEW_TAPES));
 	    if (!autolabel->template || autolabel->template == '\0') {
 		autolabel->template = NULL;
 		autolabel->autolabel = 0;
