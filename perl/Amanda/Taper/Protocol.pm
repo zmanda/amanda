e250b257fc4529456e7fa7994ac2fa6cc0bd6f3f
Jean-Louis Martineau <martineau@zmanda.com>
2010-09-14 22:25:27 +0000

    * server-src/taper.pl: Split in many files, add the worker. * perl/Amanda/Taper/Controller.pm: Splitted from taper.pl * perl/Amanda/Taper/Protocol.pm: Splitted from taper.pl * perl/Amanda/Taper/Worker.pm: New class * perl/Makefile.am: Add Controller.pm, Protocol.pm and Worker.pm.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@3402 a8d146d6-cc15-0410-8900-af154a0219e0

---
+++ perl/Amanda/Taper/Protocol.pm
@@ -0,0 +1,124 @@
+#! @PERL@
+# Copyright (c) 2009, 2010 Zmanda Inc.  All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# Contact information: Zmanda Inc., 465 S. Mathilda Ave., Suite 300
+# Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
+
+=head1 NAME
+
+Amanda::Taper::Protocol
+
+=head1 DESCRIPTION
+
+This package is a component of the Amanda taper, and is not intended for use by
+other scripts or applications.
+
+This package define the protocol between the taper and the driver, it is
+used by L<Amanda::Taper::Controller> and L<Amanda::Taper::Worker>
+
+=cut
+
+use lib '@amperldir@';
+use strict;
+use warnings;
+
+package Amanda::Taper::Protocol;
+
+use Amanda::IPC::LineProtocol;
+use base "Amanda::IPC::LineProtocol";
+
+use constant START_TAPER => message("START-TAPER",
+    format => [ qw( timestamp ) ],
+);
+
+use constant PORT_WRITE => message("PORT-WRITE",
+    format => [ qw( handle hostname diskname level datestamp
+	    dle_tape_splitsize dle_split_diskbuffer dle_fallback_splitsize dle_allow_split
+	    part_size part_cache_type part_cache_dir part_cache_max_size
+	    data_path ) ],
+);
+
+use constant FILE_WRITE => message("FILE-WRITE",
+    format => [ qw( handle filename hostname diskname level datestamp
+	    dle_tape_splitsize dle_split_diskbuffer dle_fallback_splitsize dle_allow_split
+	    part_size part_cache_type part_cache_dir part_cache_max_size
+	    orig_kb) ],
+);
+
+use constant NEW_TAPE => message("NEW-TAPE",
+    format => {
+	in => [ qw( ) ],
+	out => [ qw( handle label ) ],
+    },
+);
+
+use constant NO_NEW_TAPE => message("NO-NEW-TAPE",
+    format => {
+	in => [ qw( reason ) ],
+	out => [ qw( handle ) ],
+    }
+);
+
+use constant FAILED => message("FAILED",
+    format => {
+	in => [ qw( handle ) ],
+	out => [ qw( handle input taper inputerr tapererr ) ],
+    },
+);
+
+use constant DONE => message("DONE",
+    format => {
+	in => [ qw( handle orig_kb ) ],
+	out => [ qw( handle input taper stats inputerr tapererr ) ],
+    },
+);
+
+use constant QUIT => message("QUIT",
+    on_eof => 1,
+);
+
+use constant TAPER_OK => message("TAPER-OK",
+);
+
+use constant TAPE_ERROR => message("TAPE-ERROR",
+    format => [ qw( handle message ) ],
+);
+
+use constant PARTIAL => message("PARTIAL",
+    format => [ qw( handle input taper stats inputerr tapererr ) ],
+);
+
+use constant PARTDONE => message("PARTDONE",
+    format => [ qw( handle label fileno kb stats ) ],
+);
+
+use constant REQUEST_NEW_TAPE => message("REQUEST-NEW-TAPE",
+    format => [ qw( handle ) ],
+);
+
+use constant PORT => message("PORT",
+    format => [ qw( port ipports ) ],
+);
+
+use constant BAD_COMMAND => message("BAD-COMMAND",
+    format => [ qw( message ) ],
+);
+
+use constant DUMPER_STATUS => message("DUMPER-STATUS",
+    format => [ qw( handle ) ],
+);
+
+1;
