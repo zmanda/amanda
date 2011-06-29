4c9577229247fd7f2b60ee616e69e84edd5dc79b
Jean-Louis Martineau <martineau@zmanda.com>
2011-06-29 19:04:22 +0000

    * application-src/amstar.c: Place command_options at the and of   command line.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/branches/3_2@4199 a8d146d6-cc15-0410-8900-af154a0219e0

--- application-src/amstar.c
+++ application-src/amstar.c
@@ -1000,12 +1000,15 @@ static GPtrArray *amstar_build_argv(
 	g_ptr_array_add(argv_ptr, stralloc("-sparse"));
     g_ptr_array_add(argv_ptr, stralloc("-dodesc"));
 
+    if (command == CMD_BACKUP && argument->dle.create_index)
+	g_ptr_array_add(argv_ptr, stralloc("-v"));
+	
+    # It is best to place command_options at the and of command line.
+    # For example '-find' option requires that it is the last option used.
+    # See: http://cdrecord.berlios.de/private/man/star/star.1.html
     for (copt = argument->command_options; copt != NULL; copt = copt->next) {
 	g_ptr_array_add(argv_ptr, stralloc((char *)copt->data));
     }
-
-    if (command == CMD_BACKUP && argument->dle.create_index)
-	g_ptr_array_add(argv_ptr, stralloc("-v"));
 
     g_ptr_array_add(argv_ptr, stralloc("."));
 
