d4d078bd80187fb04d64231d1447a5030944d676
Jean-Louis Martineau <martineau@zmanda.com>
2012-05-11 12:53:41 +0000

    * common-src/glib-util.h, common-src/glib-util.c: Remove   g_queue_free_full.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/branches/3_1@4714 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/glib-util.c
+++ common-src/glib-util.c
@@ -120,15 +120,6 @@ void slist_free_full(GSList * list, GDestroyNotify free_fn) {
 }
 #endif
 
-void g_queue_free_full(GQueue * queue) {
-    while (!g_queue_is_empty(queue)) {
-        gpointer data;
-        data = g_queue_pop_head(queue);
-        amfree(data);
-    }
-    g_queue_free(queue);
-}
-
 void g_ptr_array_free_full(GPtrArray * array) {
     size_t i;
 
