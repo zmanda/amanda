58abd6b62ede2f740fe8653f3352c41972479b78
Jean-Louis Martineau <martineau@zmanda.com>
2012-06-26 14:06:27 +0000

    * device-src/s3.c: Cloudena do not have xml_version of html tag in   their reply.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@4782 a8d146d6-cc15-0410-8900-af154a0219e0

--- device-src/s3.c
+++ device-src/s3.c
@@ -1178,7 +1178,8 @@ interpret_response(S3Handle *hdl,
     if ((hdl->s3_api == S3_API_SWIFT_1 ||
 	 hdl->s3_api == S3_API_SWIFT_2) &&
 	!g_strstr_len(body, body_len, "xml version") &&
-	!g_strstr_len(body, body_len, "<html>")) {
+	!g_strstr_len(body, body_len, "<html>") &&
+	(body_len < 20 || !g_str_has_prefix(body, "<access>"))) {
 	char *body_copy = g_strndup(body, body_len);
 	char *b = body_copy;
 	char *p = strchr(b, '\n');
