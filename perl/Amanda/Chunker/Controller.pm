baae33dab286853c660171466bcbc7f9d8bc84fb
Jean-Louis Martineau <martineau@zmanda.com>
2012-10-18 11:11:54 +0000

    * All files: Change Zmanda licensing to GPLv2+ and LGPLv2+, Add Zmanda   copyright to files modified by Zmanda.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@4945 a8d146d6-cc15-0410-8900-af154a0219e0

--- perl/Amanda/Chunker/Controller.pm
+++ perl/Amanda/Chunker/Controller.pm
@@ -1,8 +1,9 @@
 # Copyright (c) 2009, 2010 Zmanda Inc.  All Rights Reserved.
 #
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published
-# by the Free Software Foundation.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
