eb446fafa0967d0c250ed467c9d7904358e3ddfd
Jean-Louis Martineau <martineau@zmanda.com>
2012-10-25 17:54:49 +0000

    * common-src/event.c: Disable -Wmissing-field-initializers when using   G_STATIC_MUTEX_INIT because of warning with glib-2.32.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@4981 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/event.c
+++ common-src/event.c
@@ -76,7 +76,10 @@ struct event_handle {
  * events and for deleting dead events */
 GSList *all_events = NULL;
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 GStaticMutex event_mutex = G_STATIC_MUTEX_INIT;
+#pragma GCC diagnostic push
 
 /* should event_loop_run stop? */
 gboolean stop = FALSE;
