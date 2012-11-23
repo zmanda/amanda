42c3b23ceea387fbdb2b21d48b3bcee8578df657
Jean-Louis Martineau <martineau@zmanda.com>
2012-11-23 13:39:46 +0000

    * device-src/null-device.c: Return DEVICE_STATUS_VOLUME_UNLABELED |   DEVICE_STATUS_VOLUME_ERROR on open for reading. * installcheck/amdevcheck.pl: Fix.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/branches/3_3@5011 a8d146d6-cc15-0410-8900-af154a0219e0

--- device-src/null-device.c
+++ device-src/null-device.c
@@ -211,7 +211,7 @@ null_device_read_label(Device * dself) {
 
     device_set_error(dself,
 	stralloc(_("Can't open NULL device for reading or appending.")),
-	DEVICE_STATUS_DEVICE_ERROR);
+	DEVICE_STATUS_VOLUME_UNLABELED | DEVICE_STATUS_VOLUME_ERROR);
     return FALSE;
 }
 
@@ -249,7 +249,7 @@ null_device_start (Device * pself, DeviceAccessMode mode,
     } else {
 	device_set_error(pself,
 	    stralloc(_("Can't open NULL device for reading or appending.")),
-	    DEVICE_STATUS_DEVICE_ERROR);
+	    DEVICE_STATUS_VOLUME_UNLABELED | DEVICE_STATUS_VOLUME_ERROR);
         return FALSE;
     }
 }
