95ecc892a82aa2cb08cec40c3bbd2f6a241863b0
Jean-Louis Martineau <martineau@zmanda.com>
2013-03-19 21:00:26 +0000

    * perl/Amanda/Changer.pm: Improve error message, * pel/Amanda/Config.swg, perl/Amanda/Util.swg: EXTEND stack when   returning an array.
    git-svn-id: https://amanda.svn.sourceforge.net/svnroot/amanda/amanda/trunk@5190 a8d146d6-cc15-0410-8900-af154a0219e0

--- perl/Amanda/Changer.pm
+++ perl/Amanda/Changer.pm
@@ -929,7 +929,7 @@ sub _new_from_uri { # (note: this sub is patched by the installcheck)
 
     my $rv = eval {$pkgname->new(Amanda::Changer::Config->new($cc), $uri,
 				 %params);};
-    die "$pkgname->new return undef" if $@;
+    die "$pkgname->new failed: $@" if $@;
     die "$pkgname->new did not return an Amanda::Changer object or an Amanda::Changer::Error"
 	unless ($rv->isa("Amanda::Changer") or $rv->isa("Amanda::Changer::Error"));
 
