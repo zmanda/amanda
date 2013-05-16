65cea43b4055f3540e8d68a1cfbed1f435cdc8ba
Jean-Louis Martineau <martineau@zmanda.com>
2013-05-16 13:04:29 +0000

    * server-src/amidxtaped.pl: untaint input variable.
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/branches/3_3@5282 a8d146d6-cc15-0410-8900-af154a0219e0

--- server-src/amidxtaped.pl
+++ server-src/amidxtaped.pl
@@ -890,7 +890,8 @@ sub getline {
 	last if $c eq "\n";
     }
 
-    my $chopped = $line;
+    $line =~ /^(.*)$/;
+    my $chopped = $1;
     $chopped =~ s/[\r\n]*$//g;
     debug("CTL << $chopped");
 
@@ -914,8 +915,8 @@ sub getline_async {
 	return $async_read_cb->($err, undef) if $err;
 
 	$buf .= $data;
-	if ($buf =~ /\r\n$/) {
-	    my $chopped = $buf;
+	if ($buf =~ /^(.*\r\n)$/) {
+	    my $chopped = $1;
 	    $chopped =~ s/[\r\n]*$//g;
 	    debug("CTL << $chopped");
 
