1350a8b375413fd4496896ba85d8fb21b5030c85
Jean-Louis Martineau <martineau@zmanda.com>
2013-08-12 20:26:39 +0000

    * server-src/amfetchdump.pl: Print applicationstdout to debug file and   stderr.
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/branches/3_3@5415 a8d146d6-cc15-0410-8900-af154a0219e0

--- server-src/amfetchdump.pl
+++ server-src/amfetchdump.pl
@@ -395,10 +395,12 @@ sub main {
     my %recovery_params;
     my $timer;
     my $is_tty;
+    my $last_is_size;
     my $delay;
     my $directtcp = 0;
     my @directtcp_command;
     my $xfer_app;
+    my $xfer_dest;
 
     my $steps = define_steps
 	cb_ref => \$finished_cb;
@@ -513,7 +515,6 @@ sub main {
 
 	# and set up the destination..
 	my $dest_fh;
-	my $xfer_dest;
 	my @filters;
 
 	if (defined $opt_data_path and $opt_data_path eq 'directtcp' and !$directtcp_supported) {
@@ -613,8 +614,8 @@ sub main {
 		debug("Running: ". join(' ',@argv));
 		$xfer_app =  Amanda::Xfer::Filter::Process->new(\@argv, 0);
 
-		$dest_fh = \*STDOUT;
-		$xfer_dest = Amanda::Xfer::Dest::Fd->new($dest_fh);
+		#$dest_fh = \*STDOUT;
+		$xfer_dest = Amanda::Xfer::Dest::Buffer->new(1048576);
 	    }
 	} elsif ($opt_pipe) {
 	    $dest_fh = \*STDOUT;
@@ -645,6 +646,10 @@ sub main {
 	$timer->set_callback(sub {
 	    my $size = $xfer_src->get_bytes_read();
 	    if ($is_tty) {
+		if (!$last_is_size) {
+		    print STDERR "\n";
+		    $last_is_size = 1;
+		}
 		print STDERR "\r" . int($size/1024) . " kb ";
 	    } else {
 		print STDERR "READ SIZE: " . int($size/1024) . " kb\n";
@@ -769,6 +774,12 @@ sub main {
 			my $line = $buffer;
 			chomp $line;
 			if (length($line) > 1) {
+			    if ($is_tty) {
+				if ($last_is_size) {
+				    print STDERR "\n";
+				    $last_is_size = 0;
+				}
+			    }
 			    print STDERR "filter stderr: $line\n";
 			    debug("filter stderr: $line");
 			}
@@ -857,9 +868,22 @@ sub main {
 
     step filter_done => sub {
 	if ($is_tty) {
-	    print STDERR "\r" . int($recovery_params{'bytes_read'}/1024) . " kb ";
+	    if (!$last_is_size) {
+		print STDERR "\n";
+	    }
+	    $last_is_size = 0;
+	    print STDERR "\r" . int($recovery_params{'bytes_read'}/1024) . " kb \n";
 	} else {
 	    print STDERR "READ SIZE: " . int($recovery_params{'bytes_read'}/1024) . " kb\n";
+	}
+	if ($xfer_dest && $xfer_dest->isa("Amanda::Xfer::Dest::Buffer")) {
+	    my $buf = $xfer_dest->get();
+	    my @lines = split "\n", $buf;
+	    foreach (@lines) {
+		next if $_ =~ /^$/;
+		print STDERR "Application stdout: $_\n";
+		debug("Application stdout: $_");
+	    }
 	}
 	@xfer_errs = (@xfer_errs, @{$recovery_params{'errors'}})
 	    if $recovery_params{'errors'};
@@ -886,7 +910,7 @@ sub main {
 	    $timer->remove();
 	    $timer = undef;
 	}
-	print STDERR "\n" if $is_tty;
+	print STDERR "\n" if $is_tty and $last_is_size;
 	return failure($err, $finished_cb) if $err;
 
 	$finished_cb->();
