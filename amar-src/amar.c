ec67ec4a860d2a8045d214c7c58a5865a6bd5d02
Jean-Louis Martineau <martineau@zmanda.com>
2014-04-10 14:59:34 +0000

    * amar-src/amar.c: Fix compilation.
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/branches/3_3@5691 a8d146d6-cc15-0410-8900-af154a0219e0

--- amar-src/amar.c
+++ amar-src/amar.c
@@ -1246,7 +1246,7 @@ amar_read_cb(
 	    file_state_t *fs = (file_state_t *)iter->data;
 	    finish_file(hp, fs, TRUE);
 	}
-	g_slist_free_full(hp->file_states, g_free);
+	slist_free_full(hp->file_states, g_free);
 	read_done(hp);
 	g_free(hp->buf);
 	g_free(hp);
@@ -1599,7 +1599,7 @@ amar_read(
 	file_state_t *fs = (file_state_t *)iter->data;
 	finish_file(&hp, fs, TRUE);
     }
-    g_slist_free_full(hp.file_states, g_free);
+    slist_free_full(hp.file_states, g_free);
     g_free(hp.buf);
 
     return success;
