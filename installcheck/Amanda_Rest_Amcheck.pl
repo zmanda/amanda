792836e01a395a6609b1fa3120500d725501ef99
Jean-Louis Martineau <martineau@zmanda.com>
2014-07-04 13:15:46 +0000

    * installcheck/Amanda_Rest_Amcheck.pl: Fix.
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/trunk@5802 a8d146d6-cc15-0410-8900-af154a0219e0

--- installcheck/Amanda_Rest_Amcheck.pl
+++ installcheck/Amanda_Rest_Amcheck.pl
@@ -59,19 +59,13 @@ my $amperldir = $Amanda::Paths::amperldir;
 my $testconf;
 
 $testconf = Installcheck::Run::setup();
-$testconf->add_dumptype("mytype", [
-    "compress" => "server",
-    "starttime" => "1830",
-    "amandad_path" => "\"/path/to/amandad\"",
-]);
-$testconf->add_dle("localhost /home mytype");
+$testconf->add_dle("localhost /home installcheck-test");
 $testconf->add_dle(<<EOF);
 localhost /home-incronly {
-    mytype
-    strategy incronly
+    installcheck-test
 }
 localhost /etc {
-    mytype
+    installcheck-test
 }
 EOF
 
@@ -81,12 +75,18 @@ config_init($CONFIG_INIT_EXPLICIT_NAME, "TESTCONF");
 my $diskfile = Amanda::Config::config_dir_relative(getconf($CNF_DISKFILE));
 my $infodir = getconf($CNF_INFOFILE);
 
-#CODE 1500001
+#CODE 28* 123
 $reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/amcheck","");
-$reply->{'body'}[2]->{'message'} = "Holding disk : disk space available, using as requested";
-$reply->{'body'}[14]->{'message'} = "Server check took 1.00 seconds";
-$reply->{'body'}[18]->{'message'} = "Client check: 1 host checked in 1.00 seconds.  1 problem found.";
-$reply->{'body'}[19]->{'message'} = "(brought to you by Amanda x.y.z)";
+foreach my $message (@{$reply->{'body'}}) {
+    if (defined $message and defined $message->{'message'}) {
+	$message->{'message'} =~ s/^NOTE: host info dir .*$/NOTE: host info dir/;
+	$message->{'message'} =~ s/^NOTE: index dir .*$/NOTE: index dir/;
+	$message->{'message'} =~ s/^Holding disk .*$/Holding disk : disk space available, using as requested/;
+	$message->{'message'} =~ s/^Server check took .*$/Server check took 1.00 seconds/;
+	$message->{'message'} =~ s/^Client check: 1 host checked in \d+.\d+ seconds.  1 problem found.$/Client check: 1 host checked in 1.00 seconds.  1 problem found./;
+	$message->{'message'} =~ s/^\(brought to you by Amanda .*$/(brought to you by Amanda x.y.z)/;
+    }
+}
 is_deeply (Installcheck::Rest::remove_source_line($reply),
     { body =>
         [ {	'source_filename' => "amcheck.c",
@@ -141,7 +141,7 @@ is_deeply (Installcheck::Rest::remove_source_line($reply),
 	  },
           {	'source_filename' => "amcheck.c",
 		'severity' => '16',
-		'message' => 'NOTE: host info dir /amanda/h1/etc/amanda/TESTCONF/curinfo/localhost does not exist',
+		'message' => 'NOTE: host info dir',
 		'code' => '2800100'
 	  },
           {	'source_filename' => "amcheck.c",
@@ -151,7 +151,7 @@ is_deeply (Installcheck::Rest::remove_source_line($reply),
 	  },
           {	'source_filename' => "amcheck.c",
 		'severity' => '16',
-		'message' => 'NOTE: index dir /amanda/h1/etc/amanda/TESTCONF/index/localhost does not exist',
+		'message' => 'NOTE: index dir',
 		'code' => '2800126'
 	  },
           {	'source_filename' => "amcheck.c",
@@ -193,450 +193,6 @@ is_deeply (Installcheck::Rest::remove_source_line($reply),
         ],
       http_code => 200,
     },
-    "No config") || diag(Data::Dumper::Dumper($reply));
-$rest->stop();
-exit;
-
-#CODE 1400007
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Rest/Dles.pm",
-		'severity' => '16',
-		'message' => 'Required \'disk\' argument is not provided.',
-		'code' => '1400009'
-	  },
-        ],
-      http_code => 404,
-    },
-    "host exists in disklist");
-
-
-#CODE 140009
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/Localhost?disk=/home","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Rest/Dles.pm",
-		'diskfile' => $diskfile,
-		'host' => 'Localhost',
-		'severity' => '16',
-		'message' => 'No such host \'Localhost\' in disklist.',
-		'code' => '1400007'
-	  },
-        ],
-      http_code => 200,
-    },
-    "No such host in disklist");
-
-#CODE 140009
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Rest/Dles.pm",
-		'severity' => '16',
-		'message' => 'No command specified: force, force_level_1, force_bump, force_no_bump.',
-		'code' => '1300030'
-	  },
-        ],
-      http_code => 200,
-    },
-    "No command");
-
-#CODE 1300003
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 0 at next run.',
-		'code' => '1300003'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force=1");
-
-#CODE 1300003
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 0 at next run.',
-		'code' => '1300003'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force=1");
-
-#CODE 1300019
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'force command for localhost:/home cleared.',
-		'code' => '1300019'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force=0");
-
-#CODE 1300021
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'no force command outstanding for localhost:/home, unchanged.',
-		'code' => '1300021'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force=0");
-
-
-#CODE 1300031
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=1&force_level_1=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Rest/Dles.pm",
-		'severity' => '16',
-		'message' => 'Only one command allowed.',
-		'code' => '1300031'
-	  },
-        ],
-      http_code => 200,
-    },
-    "force=1&force_level_1=1");
-
-#CODE 1300023
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_level_1=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 1 at next run.',
-		'code' => '1300023'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_level_1=1");
-
-#CODE 1300023
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_level_1=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 1 at next run.',
-		'code' => '1300023'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_level_1=1");
-
-#CODE 1300020
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_level_1=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'force-level-1 command for localhost:/home cleared.',
-		'code' => '1300020'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_level_1=0");
-
-#CODE 1300021
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_level_1=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'no force command outstanding for localhost:/home, unchanged.',
-		'code' => '1300021'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_level_1=0");
-
-#CODE 1300023
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_level_1=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 1 at next run.',
-		'code' => '1300023'
-	  },
-        ],
-      http_code => 200,
-    },
-    "third force_level_1=1");
-
-#CODE 1300000 and 1300003
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home FORCE-LEVEL-1 command was cleared',
-		'code' => '1300000'
-	  },
-          {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to a forced level 0 at next run.',
-		'code' => '1300003'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force=1");
-
-#CODE 1300022 and 1300025
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home FORCE command was cleared',
-		'code' => '1300022'
-	  },
-          {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to bump at next run.',
-		'code' => '1300025'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_bump=1") || diag("reply: " .Data::Dumper::Dumper($reply));
-
-#CODE 1300025
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to bump at next run.',
-		'code' => '1300025'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_bump=1");
-
-#CODE 1300027
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'bump command for localhost:/home cleared.',
-		'code' => '1300027'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_bump=0");
-
-#CODE 1300028
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'no bump command outstanding for localhost:/home, unchanged.',
-		'code' => '1300028'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_bump=0");
-
-#CODE 1300025
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to bump at next run.',
-		'code' => '1300025'
-	  },
-        ],
-      http_code => 200,
-    },
-    "third force_bump=1");
-
-#CODE 1300001 and 1300026
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_no_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home FORCE-BUMP command was cleared',
-		'code' => '1300001'
-	  },
-          {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to not bump at next run.',
-		'code' => '1300026'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_no_bump=1");
-
-#CODE 1300026
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_no_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to not bump at next run.',
-		'code' => '1300026'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_no_bump=1");
-
-#CODE 1300026
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_no_bump=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'bump command for localhost:/home cleared.',
-		'code' => '1300027'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force_no_bump=0");
-
-#CODE 1300028
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_no_bump=0","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'no bump command outstanding for localhost:/home, unchanged.',
-		'code' => '1300028'
-	  },
-        ],
-      http_code => 200,
-    },
-    "second force_no_bump=0");
-
-#CODE 1300002
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home-incronly&force=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home-incronly',
-		'severity' => '16',
-		'message' => 'localhost:/home-incronly full dump done offline, next dump will be at level 1.',
-		'code' => '1300002'
-	  },
-        ],
-      http_code => 200,
-    },
-    "first force=1 for /home-incronly");
-
-#CODE 1300026
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_no_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to not bump at next run.',
-		'code' => '1300026'
-	  },
-        ],
-      http_code => 200,
-    },
-    "third force_no_bump=1");
-
-#CODE 1300024 and 1300025
-$reply = $rest->post("http://localhost:5001/amanda/v1.0/configs/TESTCONF/dles/hosts/localhost?disk=/home&force_bump=1","");
-is_deeply (Installcheck::Rest::remove_source_line($reply),
-    { body =>
-        [ {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home FORCE-NO-BUMP command was cleared.',
-		'code' => '1300024'
-	  },
-          {	'source_filename' => "$amperldir/Amanda/Curinfo.pm",
-		'host' => 'localhost',
-		'disk' => '/home',
-		'severity' => '16',
-		'message' => 'localhost:/home is set to bump at next run.',
-		'code' => '1300025'
-	  },
-        ],
-      http_code => 200,
-    },
-    "third force_bump=1");
-
-#diag("reply: " . Data::Dumper::Dumper($reply));
+    "No config") || diag("reply: " . Data::Dumper::Dumper($reply));
 
 $rest->stop();
