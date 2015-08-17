933715e1251a28692f7d291c55c48b066b1ef043
Jean-Louis Martineau <martineau@zmanda.com>
2015-08-17 13:30:58 +0000

    * common-src/amutil.c (interruptible_accept): Take a timeout as   argument * common-src/amutil.h (interruptible_accept): New prototype * common-src/stream.c (stream_client): Call interruptible_accept with   the timeout argument * xfer-src/element-glue.c (do_directtcp_accept): Call   interruptible_accept with the timeout argument
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/trunk@6305 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/amutil.c
+++ common-src/amutil.c
@@ -344,7 +344,8 @@ interruptible_accept(
     struct sockaddr *addr,
     socklen_t *addrlen,
     gboolean (*prolong)(gpointer data),
-    gpointer prolong_data)
+    gpointer prolong_data,
+    time_t timeout)
 {
     SELECT_ARG_TYPE readset;
     struct timeval tv;
@@ -358,8 +359,12 @@ interruptible_accept(
     memset(&readset, 0, sizeof(readset));
 
     while (1) {
-	if (!prolong(prolong_data)) {
+	if (prolong && !prolong(prolong_data)) {
 	    errno = 0;
+	    return -1;
+	}
+	if (time(NULL) > timeout) {
+	    errno = ETIMEDOUT;
 	    return -1;
 	}
 
@@ -368,7 +373,7 @@ interruptible_accept(
 
 	/* try accepting for 1s */
 	memset(&tv, 0, sizeof(tv));
-    	tv.tv_sec = 1;
+	tv.tv_sec = 1;
 
 	nfound = select(sock+1, &readset, NULL, NULL, &tv);
 	if (nfound < 0) {
