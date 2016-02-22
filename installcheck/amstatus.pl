4e08901a3d4c8592989c535eb0063d4c2f8e9381
Jean-Louis Martineau <martineau@zmanda.com>
2016-02-22 18:43:29 +0000

    * installcheck/amstatus.pl: Test number printing * installcheck/Makefile.am: Add catalogs/number.cat * installcheck/catalogs/number.cat: New catalog file for testing   number
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/trunk@6565 a8d146d6-cc15-0410-8900-af154a0219e0

--- installcheck/amstatus.pl
+++ installcheck/amstatus.pl
@@ -17,7 +17,7 @@
 # Contact information: Zmanda Inc, 465 S. Mathilda Ave., Suite 300
 # Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
 
-use Test::More tests => 12;
+use Test::More tests => 20;
 use strict;
 use warnings;
 
@@ -31,6 +31,83 @@ use Amanda::Constants;
 my $cat;
 my $testconf = Installcheck::Run::setup();
 $testconf->write();
+
+## try number formating
+
+$cat = Installcheck::Catalogs::load('number');
+$cat->install();
+
+ok(run('amstatus', 'TESTCONF', '-odisplayunit=k'),
+    "plain amstatus runs without error with -odisplayunit=k");
+like($Installcheck::Run::stdout,
+    qr{localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*155584k\s*flushed\s*\(11:41:56\)
+localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*155584k\s*dump done, written\s*\(11:55:09\)
+localhost.localdomain:1000g\s*[0-9]{14}\s*0\s*1048580000k\s*dump to tape done\s*\(11:42:07\)
+localhost.localdomain:100g\s*[0-9]{14}\s*0\s*104858000k\s*dump to tape done\s*\(11:42:22\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*102410k\s*flushed\s*\(11:41:56\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*102410k\s*dump done, written\s*\(11:55:03\)
+localhost.localdomain:100t\s*[0-9]{14}\s*0\s*104857700000k\s*dump to tape done\s*\(11:49:22\)
+localhost.localdomain:10g\s*[0-9]{14}\s*0\s*10486016k\s*dump to tape done\s*\(11:54:59\)
+localhost.localdomain:10m\s*[0-9]{14}\s*0\s*10250k\s*dump done, written\s*\(11:41:58\)
+localhost.localdomain:10t\s*[0-9]{14}\s*0\s*10485770000k\s*dump to tape done\s*\(11:53:14\)
+localhost.localdomain:1g\s*[0-9]{14}\s*0\s*1048755k\s*dump to tape done\s*\(11:42:54\)
+localhost.localdomain:2ga\s*[0-9]{14}\s*0\s*2097160k\s*dump to tape done\s*\(11:43:35\)
+localhost.localdomain:5ga\s*[0-9]{14}\s*0\s*5242890k\s*dump to tape done\s*\(11:45:36\)},
+    "output is reasonable with -odisplayunit=k");
+
+ok(run('amstatus', 'TESTCONF', '-odisplayunit=m'),
+    "plain amstatus runs without error with -odisplayunit=m");
+like($Installcheck::Run::stdout,
+    qr{localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*151m\s*flushed\s*\(11:41:56\)
+localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*151m\s*dump done, written\s*\(11:55:09\)
+localhost.localdomain:1000g\s*[0-9]{14}\s*0\s*1024003m\s*dump to tape done\s*\(11:42:07\)
+localhost.localdomain:100g\s*[0-9]{14}\s*0\s*102400m\s*dump to tape done\s*\(11:42:22\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*100m\s*flushed\s*\(11:41:56\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*100m\s*dump done, written\s*\(11:55:03\)
+localhost.localdomain:100t\s*[0-9]{14}\s*0\s*102400097m\s*dump to tape done\s*\(11:49:22\)
+localhost.localdomain:10g\s*[0-9]{14}\s*0\s*10240m\s*dump to tape done\s*\(11:54:59\)
+localhost.localdomain:10m\s*[0-9]{14}\s*0\s*10m\s*dump done, written\s*\(11:41:58\)
+localhost.localdomain:10t\s*[0-9]{14}\s*0\s*10240009m\s*dump to tape done\s*\(11:53:14\)
+localhost.localdomain:1g\s*[0-9]{14}\s*0\s*1024m\s*dump to tape done\s*\(11:42:54\)
+localhost.localdomain:2ga\s*[0-9]{14}\s*0\s*2048m\s*dump to tape done\s*\(11:43:35\)
+localhost.localdomain:5ga\s*[0-9]{14}\s*0\s*5120m\s*dump to tape done\s*\(11:45:36\)},
+    "output is reasonable with -odisplayunit=m");
+
+ok(run('amstatus', 'TESTCONF', '-odisplayunit=g'),
+    "plain amstatus runs without error with -odisplayunit=g");
+like($Installcheck::Run::stdout,
+    qr{localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*0g\s*flushed\s*\(11:41:56\)
+localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*0g\s*dump done, written\s*\(11:55:09\)
+localhost.localdomain:1000g\s*[0-9]{14}\s*0\s*1000g\s*dump to tape done\s*\(11:42:07\)
+localhost.localdomain:100g\s*[0-9]{14}\s*0\s*100g\s*dump to tape done\s*\(11:42:22\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*0g\s*flushed\s*\(11:41:56\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*0g\s*dump done, written\s*\(11:55:03\)
+localhost.localdomain:100t\s*[0-9]{14}\s*0\s*100000g\s*dump to tape done\s*\(11:49:22\)
+localhost.localdomain:10g\s*[0-9]{14}\s*0\s*10g\s*dump to tape done\s*\(11:54:59\)
+localhost.localdomain:10m\s*[0-9]{14}\s*0\s*0g\s*dump done, written\s*\(11:41:58\)
+localhost.localdomain:10t\s*[0-9]{14}\s*0\s*10000g\s*dump to tape done\s*\(11:53:14\)
+localhost.localdomain:1g\s*[0-9]{14}\s*0\s*1g\s*dump to tape done\s*\(11:42:54\)
+localhost.localdomain:2ga\s*[0-9]{14}\s*0\s*2g\s*dump to tape done\s*\(11:43:35\)
+localhost.localdomain:5ga\s*[0-9]{14}\s*0\s*5g\s*dump to tape done\s*\(11:45:36\)},
+    "output is reasonable with -odisplayunit=g");
+
+ok(run('amstatus', 'TESTCONF', '-odisplayunit=t'),
+    "plain amstatus runs without error with -odisplayunit=t");
+like($Installcheck::Run::stdout,
+    qr{localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*0t\s*flushed\s*\(11:41:56\)
+localhost.localdomain:/bootAMGTAR\s*[0-9]{14}\s*0\s*0t\s*dump done, written\s*\(11:55:09\)
+localhost.localdomain:1000g\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:42:07\)
+localhost.localdomain:100g\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:42:22\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*0t\s*flushed\s*\(11:41:56\)
+localhost.localdomain:100m\s*[0-9]{14}\s*0\s*0t\s*dump done, written\s*\(11:55:03\)
+localhost.localdomain:100t\s*[0-9]{14}\s*0\s*97t\s*dump to tape done\s*\(11:49:22\)
+localhost.localdomain:10g\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:54:59\)
+localhost.localdomain:10m\s*[0-9]{14}\s*0\s*0t\s*dump done, written\s*\(11:41:58\)
+localhost.localdomain:10t\s*[0-9]{14}\s*0\s*9t\s*dump to tape done\s*\(11:53:14\)
+localhost.localdomain:1g\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:42:54\)
+localhost.localdomain:2ga\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:43:35\)
+localhost.localdomain:5ga\s*[0-9]{14}\s*0\s*0t\s*dump to tape done\s*\(11:45:36\)},
+    "output is reasonable with -odisplayunit=t");
 
 ## try a few various options with a pretty normal logfile
 
