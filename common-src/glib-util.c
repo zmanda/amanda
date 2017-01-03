93cd364adacf0854af4c4132d8c7638e9cd11d17
Jean-Louis Martineau <martineau@zmanda.com>
2017-01-03 16:28:29 +0000

    * common-src/glib-util.c: Make sure to use the feature test macro for   LIBCURL before trying to use either the GNUTLS or OPENSSL feature   test macros
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/trunk@6991 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/glib-util.c
+++ common-src/glib-util.c
@@ -37,7 +37,7 @@
 #include <curl/curl.h>
 #endif
 
-#if defined LIBCURL_USE_OPENSSL || defined SSL_SECURITY
+#if (defined HAVE_LIBCURL && defined LIBCURL_USE_OPENSSL) || defined SSL_SECURITY
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
@@ -75,7 +75,7 @@ init_openssl(void)
 
 #endif /* LIBCURL_USE_OPENSSL */
 
-#if defined LIBCURL_USE_GNUTLS_GCRYPT
+#if defined HAVE_LIBCURL && defined LIBCURL_USE_GNUTLS_GCRYPT
 
 #include <gcrypt.h>
 #include <errno.h>
@@ -98,10 +98,10 @@ static void
 init_ssl(void)
 {
 
-#if defined LIBCURL_USE_OPENSSL || defined SSL_SECURITY
+#if (defined HAVE_LIBCURL && defined LIBCURL_USE_OPENSSL) || defined SSL_SECURITY
     init_openssl();
 #endif
-#if defined LIBCURL_USE_GNUTLS_GCRYPT
+#if defined HAVE_LIBCURL && defined LIBCURL_USE_GNUTLS_GCRYPT
     init_gnutls_gcrypt();
 #endif
 }
