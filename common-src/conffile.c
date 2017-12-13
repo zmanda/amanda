87b43f788c17fa658f2f6f95c6b29053232d0cf7
Jean-Louis Martineau <martineau@zmanda.com>
2017-12-13 19:53:31 +0000

    * common-src/conffile.c: Accept define with double quote
    git-svn-id: https://svn.code.sf.net/p/amanda/code/amanda/branches/3_5@7571 a8d146d6-cc15-0410-8900-af154a0219e0

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -1914,6 +1914,7 @@ negative_number: /* look for goto negative_number below sign is set there */
 
 	    tok = (token_overflow) ? CONF_UNKNOWN :
 			(exp == CONF_IDENT) ? CONF_IDENT : CONF_STRING;
+	    tokenval.type = (exp == CONF_IDENT) ? CONFTYPE_IDENT : CONFTYPE_STR;
 	    break;
 
 	case '-':
@@ -5670,7 +5671,7 @@ validate_no_space_dquote(
 	break;
       }
     default:
-	conf_parserror("validate_no_space_dquote invalid type %d\n", val->type);
+	conf_parserror("validate_name invalid type %d\n", val->type);
     }
 }
 
