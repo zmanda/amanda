47b60c585af1a84e4bf96a55270bf63538fe44e9
Chris Hassell <chris.hassell@betsol.com>
2019-11-05 13:16:54 -0700

    Fix various lintian errors relating to man pages, including line breaks, non-standard FHS paths, etc. -- Bill Blough <devel@blough.us>

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -6424,9 +6424,9 @@ init_defaults(
     conf_init_str(&conf_data[CNF_CHANGERFILE]             , "changer");
     conf_init_str   (&conf_data[CNF_TAPELIST]             , "tapelist");
     conf_init_str   (&conf_data[CNF_DISKFILE]             , "disklist");
-    conf_init_str   (&conf_data[CNF_INFOFILE]             , "/usr/adm/amanda/curinfo");
-    conf_init_str   (&conf_data[CNF_LOGDIR]               , "/usr/adm/amanda");
-    conf_init_str   (&conf_data[CNF_INDEXDIR]             , "/usr/adm/amanda/index");
+    conf_init_str   (&conf_data[CNF_INFOFILE]             , CONFIG_DIR "/curinfo");
+    conf_init_str   (&conf_data[CNF_LOGDIR]               , CONFIG_DIR);
+    conf_init_str   (&conf_data[CNF_INDEXDIR]             , CONFIG_DIR "/index");
     conf_init_ident    (&conf_data[CNF_TAPETYPE]             , "DEFAULT_TAPE");
     conf_init_identlist(&conf_data[CNF_HOLDINGDISK]          , NULL);
     conf_init_int      (&conf_data[CNF_DUMPCYCLE]            , CONF_UNIT_NONE, 10);
