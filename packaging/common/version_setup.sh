6f7e2f6d66fbd31a6f9a8b4eee34b6b335589bf0
Chris Hassell <chris.hassell@betsol.com>
2021-05-19 09:11:13 -0600

    a nice script to provide version numbers based on the git

--- packaging/common/version_setup.sh
+++ packaging/common/version_setup.sh
@@ -63,7 +63,7 @@ detect_git_cache() {
 get_latest_git_tag() {
     local ref="$1"
     local sha="$(git 2>/dev/null rev-parse --verify $ref)"
-    local tags="$(git show-ref --tags | sed -e "\|^$sha |!d" -e 's,.* refs/tags/,,')"
+    local tags="$(git show-ref --dereference --tags | sed -e "\|^$sha |!d" -e 's,.* refs/tags/,,' -e 's,\^{}$,,')"
     [ -n "$tags" ] || return;
     {
        # prioritize fully-numeric versions of latest numeric order by decimal
@@ -150,13 +150,13 @@ get_git_info() {
         tmpsubject="$(git log -1 --pretty=%s $oref)"
         [[ "$tmpsubject" != Merge\ remote-tracking\ branch*into\ $tmpbranch ]] && break
 
-        newsha="$(git rev-parse 2>/dev/null --verify --short ${oref}^2)"
+        newsha="$(git rev-parse 2>/dev/null --verify --short=9 ${oref}^2)"
         [ -z "$newsha" ] && break
 
         ref=$newsha
     done
 
-    declare -g SHA=$(git rev-parse --short $oref)
+    declare -g SHA=$(git rev-parse --short=9 $oref)
 
     local year_break
 
@@ -170,6 +170,7 @@ get_git_info() {
     local REV_REFBR
     local REV_REFDIST
 
+    # exact matches...
     REV_TAGPOS=${REV_TAGPOS:-$(get_latest_git_tag $oref)}
     REV_TAGROOT=$(git describe --tags $oref 2>/dev/null)
     REV_TAGPOS="${REV_TAGPOS%^[0-9]*}"
@@ -177,21 +178,13 @@ get_git_info() {
     # trim off tag-dist
     [ -n "$REV_TAGDIST" ] && REV_TAGROOT="${REV_TAGROOT%-$REV_TAGDIST-g[a-f0-9]*}"
 
-    # get name-rev branch-based name and distance (and prefer certain branches if possible)
-    REF_IDEAL=$(git name-rev --name-only --refs='origin/next*[0-9].[0-9]*' $oref)
-    REF_IDEAL="${REF_IDEAL#undefined}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --refs='origin/stable*[0-9].[0-9]*' $oref)}"
-    REF_IDEAL="${REF_IDEAL#undefined}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --refs='origin/integ*[0-9].[0-9]*' $oref)}"
-    REF_IDEAL="${REF_IDEAL#undefined}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --refs='origin/dev*[0-9].[0-9]*' $oref)}"
-    REF_IDEAL="${REF_IDEAL#undefined}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --exclude=HEAD --refs='origin/*[0-9].[0-9]*' $oref 2>/dev/null)}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --refs='origin/*[0-9].[0-9]*' $oref)}"
-    REF_IDEAL="${REF_IDEAL#undefined}"
-    REF_IDEAL="${REF_IDEAL:-$(git name-rev --name-only --refs='origin/*[0-9][0-9][0-9]*' $oref)}"
-    REF_IDEAL="${REF_IDEAL#undefined}"
-
+    # prefer certain branches if multiples match
+    # get describe branch-matching name
+    REF_IDEAL="${REF_IDEAL:-$(git 2>/dev/null describe --all --exact-match --match 'origin/R-[Rr]elease*[0-9].[0-9]*' $oref)}" #1
+    REF_IDEAL="${REF_IDEAL:-$(git 2>/dev/null describe --all --exact-match --match 'origin/[FH]-*[0-9].[0-9]*' $oref)}" #2
+    REF_IDEAL="${REF_IDEAL:-$(git 2>/dev/null describe --all --exact-match --match 'origin/[D]-*[0-9].[0-9]*' $oref)}" #2
+    REF_IDEAL="${REF_IDEAL:-$(git 2>/dev/null describe --all --exact-match --match 'origin/[a-z]*[a-z]-[0-9].[0-9]*' $oref)}" #3
+    REF_IDEAL="${REF_IDEAL:-$(git 2>/dev/null describe --all --exact-match --match 'origin/*[0-9].[0-9]*' $oref)}" #5
     REF_IDEAL="${REF_IDEAL%^0}"
 
     # get branch name separated out and get distance-back from branch (or zero)
@@ -201,8 +194,8 @@ get_git_info() {
 
     if [ -n "$REV_REFBR" -a $(( ${pkgtime_name:0:2} + 2000 )) -gt 2000 ]; then
         year_break=$(printf %04d-01-01 $(( ${pkgtime_name:0:2} + 2000 )) )
-        read REV_YRDIST < <(git log --pretty='%H' --until $year_break $REV_REFBR | 
-                             git name-rev --name-only --refs=$REV_REFBR --stdin | 
+        read REV_YRDIST < <(TZ=GMT git log --pretty='%H' --until $year_break $REV_REFBR |
+                             git name-rev --name-only --refs=$REV_REFBR --stdin |
                              sed -r -e '/^[^~]*\~[0-9]+$/!d' -e q)
         REV_YRDIST=${REV_YRDIST##*~}
         REV_YRDIST=${REV_YRDIST%%^*}  # clean up if needed
@@ -210,18 +203,23 @@ get_git_info() {
         REV_YRDIST=$(printf %04d $(( REV_YRDIST - REV_REFDIST )))
         REV_YRDIST=.y${year_code}+${REV_YRDIST%%^*}  # clean up if needed
     fi
-    
+
     ####### officially prove a tag or branch are reachable as a base
 
     # test if we have a specific (master) branch
-    REV_PLACE="$(git rev-parse --short --symbolic-full-name "$REV_TAGPOS" 2>/dev/null)"   # get short hash-version
-    REV_PLACE=${REV_PLACE:-"$(git rev-parse --short --symbolic-full-name "$REV_REFBR" 2>/dev/null)"}
+    REV_PLACE="$(git rev-parse --short=9 --symbolic-full-name "$REV_TAGPOS" 2>/dev/null)"   # get short hash-version
+    REV_PLACE=${REV_PLACE:-"$(git rev-parse --short=9 --symbolic-full-name "$REV_REFBR" 2>/dev/null)"}
 
     unset GIT_DIR
     #### END OF GIT_DIR ENVIRONMENT OVERRIDE (IF PRESENT) #####
     ############################################################
     # check if not using same location as head?
     REV_SUFFIX=
+
+    # in case this is the only difference...
+    GIT_WORKING_DIR=${src_root} git reset ${src_root}/.gitmodules
+    GIT_WORKING_DIR=${src_root} git checkout ${src_root}/.gitmodules
+
     if [ "$(git rev-parse $ref)" != "$(git rev-parse HEAD)" ]; then
 	:
     # check if zero diff found in files anywhere?
@@ -239,7 +237,8 @@ get_git_info() {
         BRANCH="$REV_TAGPOS"
         LONG_BRANCH="tags/$BRANCH"
         REV=""       # no extra to branch info
-        PKG_REV="1"  # precise name for it.. use no hash
+        PKG_REV="dev"  # precise name for it.. use no hash
+        [ "${BUILD_FLAG}" = "release" ] && PKG_REV=1
 
     # build is simply from master
     elif [ "$REV_PLACE" = "refs/heads/master" ]; then
@@ -255,7 +254,7 @@ get_git_info() {
         REV="$REV_YRDIST"         # add commit# for year
         PKG_REV="${pkgtime_name:3}.git.$SHA"
 
-    # build is not on a branch, so name it with tag and tag-distance 
+    # build is not on a branch, so name it with tag and tag-distance
     elif [ -n "$REV_TAGROOT" ] && [ 0$REV_TAGDIST -gt 0 ]; then
         BRANCH="$REV_TAGROOT"
         LONG_BRANCH="tags/$BRANCH"
@@ -269,24 +268,64 @@ get_git_info() {
 
     # default branch name
     BRANCH="${BRANCH:-git}"
-
-    BRANCH="${BRANCH//-/.}"  # remove - for branch name (not allowed)
-    BRANCH="${BRANCH//_/.}"  # remove _ for branch name (not allowed)
 }
 
 branch_version_name() {
-    local v="$1"
-    local vv
+    local br="$1"
+    local pre
+    local post
+    local ver
 
-    # strip any path and remove trailing text from 'git describe'
-    v=$(sed <<<"${v##*/}" -r -e 's,-[0-9]+-g[a-f0-9]+$,,')
-    # change _ or . to an unused char '^' 
-    v="${v//[_-]/^}"
-    # remove text before version number (numerals before a ^ char)
-    vv=${v%%[0-9]*}
-    v=${v:${#vv}}
-    # apply ^ replacement with . [numbers on both sides] repeatedly until none more found
-    echo "${v//^/.}"
+    # strip any path 
+    br="${br##*/}"
+    # remove overspecific text from 'git describe'
+    br=$(sed <<<"$br" -r -e 's,-[0-9]+-g[a-f0-9]+$,,')
+    # change - to _ until _ is changed to .
+    # measure text before version number (last number)
+    pre="${br%%[0-9]*}";
+    # measure text after version number (first after number-or-dot)
+    post="${br#*[0-9]}-"; post="${post#*[^0-9.]}"; 
+    post="${br:$(( ${#br} - ${#post}))}"
+
+    # if no version numbers available reject this name ...
+    if [ $pre = $post ]; then return; fi            ###### RETURN
+
+    # apply _ replacement with . [numbers on both sides] repeatedly until none more found
+    [ -n "${post}" ] && ver=${br:${#pre}:-${#post}}  # skip post
+    [ -z "${post}" ] && ver=${br:${#pre}}            # all
+
+    [[ "$pre" == [D]-??*- ]] && ver+=".${pre%-}"
+    [[ "$pre" == [FH]-* ]] &&   ver+=".${pre:0:1}"
+    [[ "$pre" == [a-z]*- ]] && [[ "$pre" != *-[^-]* ]] && ver+=".${pre:0:1}"
+
+    ver+="${post,,}"
+    if [ "$ver" != "${ver:0:31}" ]; then 
+        ver="${ver:0:31}" 
+        ver="${ver%[-_.]*}"
+    fi
+    ver="${ver//[-_]/.}"
+    echo "${ver}"
+}
+
+set_build_version() {
+    local top=$(git rev-parse --show-superproject-working-tree)
+    declare -g BUILD_VERSION
+
+    top=${top:-$(git rev-parse --show-toplevel)}
+    top=${top:-.}
+    top=$(realpath $top)
+
+    read pkgbr < <(git 2>/dev/null config --blob ${SHA}:.gitmodules --get submodule.packaging.branch)
+    read toolbr < <(git 2>/dev/null config --blob ${SHA}:.gitmodules --get submodule.tools-archives.branch)
+
+    pkg_pkgdir="--git-dir=${top}/.git/modules/packaging"
+    tools_pkgdir="--git-dir=${top}/.git/modules/tools-archives"
+
+    command git $pkg_pkgdir rev-parse --quiet origin/$pkgbr &&
+        read pkgbr < <(git $pkg_pkgdir rev-parse --verify --short=9 origin/$pkgbr)
+    command git $tools_pkgdir rev-parse --verify --quiet origin/$toolbr &&
+        read toolbr < <(git $tools_pkgdir rev-parse --verify --short=9 origin/$toolbr)
+    declare -g BUILD_VERSION="${SHA}.${pkgbr}.${toolbr}"
 }
 
 old_set_pkg_rev() {
@@ -322,6 +361,7 @@ save_version() {
     ref=$1
     # quiet!  no output until end
     get_git_info $ref >/dev/null
+    set_build_version >/dev/null
 
     VERSION=$(branch_version_name "$BRANCH")
     [ "${BRANCH}" = "trunk" ] && VERSION="trunk"
@@ -341,6 +381,7 @@ save_version() {
         mkdir -p $repo_vers_dir
 
         echo -n $VERSION > $repo_vers_dir/FULL_VERSION
+        echo -n $BUILD_VERSION > $repo_vers_dir/BUILD_VERSION
         echo -n "${PKG_REV:-1}" > $repo_vers_dir/PKG_REV
         echo -n $LONG_BRANCH > $repo_vers_dir/LONG_BRANCH
         echo -n $REV > $repo_vers_dir/REV
@@ -353,7 +394,7 @@ save_version() {
 	sed -e 's,^declare --,declare -g,';
     [ -n "$PKG_NAME_VER" ] && declare -p PKG_NAME_VER |
 	sed -e 's,^declare --,declare -g,';
-    declare -p VERSION PKG_REV |
+    declare -p VERSION BUILD_VERSION PKG_REV |
 	sed -e 's,^declare --,declare -g,';
     declare -p LONG_BRANCH BRANCH REV |
 	sed -e 's,^declare --,declare -g,';
@@ -363,11 +404,10 @@ save_version() {
 # Fall back to previous build (or dist build?)?
 if ! [ -e .git ]; then
     [ -f FULL_VERSION -a -f PKG_REV ] && exit 0
-    echo "Error: No subversion or git info available!"   #### ERROR
+    echo "Error: $(pwd): No subversion or git info available!"   #### ERROR
     exit 1
 fi
 
 git remote -v show > vcs_repo.info
 git --no-pager log --max-count=1 >> vcs_repo.info
 save_version ${1:-HEAD}
-
