da591abb96e89bad878199a82009061bfd362932
Chris Hassell <>
2022-05-23 13:27:38 -0600

    feature to prevent erasure of physical tapes due to autolabel options.. without compile-time option

--- common-src/conffile.c
+++ common-src/conffile.c
@@ -180,8 +180,11 @@ typedef enum {
     CONF_CUSTOM,
 
     /* autolabel */
-    CONF_AUTOLABEL,		CONF_ANY_VOLUME,	CONF_OTHER_CONFIG,
-    CONF_NON_AMANDA,		CONF_VOLUME_ERROR,	CONF_EMPTY,
+    CONF_AUTOLABEL,		CONF_ANY_VOLUME,
+#if WANT_UNSAFE_AUTOLABEL
+    CONF_OTHER_CONFIG,          CONF_NON_AMANDA,
+#endif
+    CONF_VOLUME_ERROR,	        CONF_EMPTY,
     CONF_META_AUTOLABEL,
 
     /* labelstr */
@@ -1133,12 +1136,16 @@ keytab_t server_keytab[] = {
     { "NOFULL", CONF_NOFULL },
     { "NOINC", CONF_NOINC },
     { "NONE", CONF_NONE },
+#if WANT_UNSAFE_AUTOLABEL
     { "NON_AMANDA", CONF_NON_AMANDA },
+#endif
     { "OPTIONAL", CONF_OPTIONAL },
     { "ORDER", CONF_ORDER },
     { "ORG", CONF_ORG },
     { "OTHER", CONF_OTHER },
+#if WANT_UNSAFE_AUTOLABEL
     { "OTHER_CONFIG", CONF_OTHER_CONFIG },
+#endif
     { "PART_CACHE_DIR", CONF_PART_CACHE_DIR },
     { "PART_CACHE_MAX_SIZE", CONF_PART_CACHE_MAX_SIZE },
     { "PART_CACHE_TYPE", CONF_PART_CACHE_TYPE },
@@ -2100,11 +2107,18 @@ read_conffile(
     current_filename = get_seen_filename(filename);
     amfree(filename);
 
+    if (!save_filename) { current_line_num = 0; }
+
     if ((current_file = fopen(current_filename, "r")) == NULL) {
-	if (!missing_ok || errno != ENOENT)
-	    conf_parserror(_("could not open conf file '%s': %s"),
-		    current_filename, strerror(errno));
-	goto finish;
+        if (missing_ok && errno == ENOENT)
+            goto finish;
+        if (save_filename) {
+            conf_parserror(_("could not include conf file '%s' from '%s': %s"),
+                current_filename, save_filename, strerror(errno));
+        }
+        conf_parserror(_("could not open conf file '%s': %s"),
+                current_filename, strerror(errno));
+        goto finish;
     }
     g_debug("reading config file %s", current_filename);
 
@@ -5089,16 +5103,22 @@ read_autolabel(
 	if (tok == CONF_ANY_VOLUME)
 	    val->v.autolabel.autolabel |= AL_OTHER_CONFIG | AL_NON_AMANDA |
 					  AL_VOLUME_ERROR | AL_EMPTY;
+#if WANT_UNSAFE_AUTOLABEL
 	else if (tok == CONF_OTHER_CONFIG)
 	    val->v.autolabel.autolabel |= AL_OTHER_CONFIG;
 	else if (tok == CONF_NON_AMANDA)
 	    val->v.autolabel.autolabel |= AL_NON_AMANDA;
+#endif
 	else if (tok == CONF_VOLUME_ERROR)
 	    val->v.autolabel.autolabel |= AL_VOLUME_ERROR;
 	else if (tok == CONF_EMPTY)
 	    val->v.autolabel.autolabel |= AL_EMPTY;
 	else {
-	    conf_parserror(_("ANY, NEW-VOLUME, OTHER-CONFIG, NON-AMANDA, VOLUME-ERROR or EMPTY is expected"));
+	    conf_parserror(_("ANY, NEW-VOLUME, "
+#if WANT_UNSAFE_AUTOLABEL
+                           "OTHER-CONFIG, NON-AMANDA, "
+#endif
+                           "VOLUME-ERROR or EMPTY is expected"));
 	}
 	get_conftoken(CONF_ANY);
     }
@@ -6476,7 +6496,7 @@ init_defaults(
     conf_init_identlist(&conf_data[CNF_STORAGE]              , NULL);
     conf_init_identlist(&conf_data[CNF_VAULT_STORAGE]        , NULL);
     conf_init_str      (&conf_data[CNF_CMDFILE]              , "command_file");
-    conf_init_int      (&conf_data[CNF_REST_API_PORT]        , CONF_UNIT_NONE, 0);
+    conf_init_int      (&conf_data[CNF_REST_API_PORT]        , CONF_UNIT_NONE, 5000);
     conf_init_str      (&conf_data[CNF_REST_SSL_CERT]        , NULL);
     conf_init_str      (&conf_data[CNF_REST_SSL_KEY]         , NULL);
     conf_init_bool     (&conf_data[CNF_USETIMESTAMPS]        , 1);
